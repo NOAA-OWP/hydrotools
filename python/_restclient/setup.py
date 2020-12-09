f643a9c2cfd2020377806bcdc138bf6186992ff5
Austin Raney <aaraney@crimson.ua.edu>
2020-12-09 10:57:49 -0500

    add _restclient supporting setuptools files

---
+++ python/_restclient/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_namespace_packages
+
+# python namespace subpackage
+# this namespace package follows PEP420
+# https://packaging.python.org/guides/packaging-namespace-packages/#native-namespace-packages
+
+NAMESPACE_PACKAGE_NAME = "evaluation_tools"
+SUBPACKAGE_NAME = "_restclient"
+
+# Namespace subpackage slug.
+# Ex: mypkg.a # Where the namespace pkg = `mypkg` and the subpackage = `a`
+SUBPACKAGE_SLUG = f"{NAMESPACE_PACKAGE_NAME}.{SUBPACKAGE_NAME}"
+
+# Subpackage version
+VERSION = "1.0.0"
+
+# Package author information
+AUTHOR = "Austin Raney"
+AUTHOR_EMAIL = "arthur.raney@noaa.gov"
+
+# Short sub-package description
+DESCRIPTION = "Abstract REST client with built in request caching and retries"
+
+# Package dependency requirements
+REQUIREMENTS = ["requests", "requests_cache"]
+
+setup(
+    name=SUBPACKAGE_SLUG,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    classifiers=[
+        "Private :: Do Not Upload to pypi server",
+    ],
+    description=DESCRIPTION,
+    namespace_packages=[NAMESPACE_PACKAGE_NAME],
+    packages=find_namespace_packages(include=[f"{NAMESPACE_PACKAGE_NAME}.*"]),
+    install_requires=REQUIREMENTS,
+)
