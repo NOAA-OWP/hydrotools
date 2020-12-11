b024b1995c5b3142aeb540384e02243f0ea4b2fc
Jason Regina <jarq6c@gmail.com>
2020-12-11 12:31:43 -0600

    broaden test coverage

--- python/gcp_client/tests/test_gcp.py
+++ python/gcp_client/tests/test_gcp.py
@@ -1,5 +1,6 @@
 import pytest
 from evaluation_tools.gcp_client import gcp
+from os import cpu_count
 
 @pytest.fixture
 def setup_gcp():
@@ -7,3 +8,17 @@ def setup_gcp():
 
 def test_bucket_name(setup_gcp):
     assert (setup_gcp.bucket_name) == "national-water-model"
+
+def test_max_processes(setup_gcp):
+    count = cpu_count() - 2
+    assert (setup_gcp.max_processes) == count
+
+@pytest.mark.slow
+def test_get(setup_gcp):
+    # TODO
+    """Test data retrieval and parsing"""
+    df = setup_gcp.get(
+        configuration='analysis_assim_extend',
+        reference_time='20201209T16Z'
+    )
+    assert not df.empty
