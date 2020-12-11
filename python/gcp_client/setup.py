5dc8bb2d54c63ced95475fbcf727d4e4ba5aa24a
Jason Regina <jarq6c@gmail.com>
2020-12-11 08:47:58 -0600

    New client tool to retrieve NWM data from google cloud platform

---
+++ python/gcp_client/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_namespace_packages
+
+# python namespace subpackage
+# this namespace package follows PEP420
+# https://packaging.python.org/guides/packaging-namespace-packages/#native-namespace-packages
+
+NAMESPACE_PACKAGE_NAME = "evaluation_tools"
+SUBPACKAGE_NAME = "gcp_client"
+
+# Namespace subpackage slug.
+# Ex: mypkg.a # Where the namespace pkg = `mypkg` and the subpackage = `a`
+SUBPACKAGE_SLUG = f"{NAMESPACE_PACKAGE_NAME}.{SUBPACKAGE_NAME}"
+
+# Subpackage version
+VERSION = "0.1.0"
+
+# Package author information
+AUTHOR = "Jason Regina"
+AUTHOR_EMAIL = "jason.regina@noaa.gov"
+
+# Short sub-package description
+DESCRIPTION = "Retrieve National Water Model data from Google Cloud Platform."
+
+# Package dependency requirements
+REQUIREMENTS = [
+    "pandas",
+    "xarray",
+    "google-cloud-storage",
+    "h5netcdf"
+]
+
+setup(
+    name=SUBPACKAGE_SLUG,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    classifiers=[
+        "",
+    ],
+    description=DESCRIPTION,
+    namespace_packages=[NAMESPACE_PACKAGE_NAME],
+    packages=find_namespace_packages(include=[f"{NAMESPACE_PACKAGE_NAME}.*"]),
+    install_requires=REQUIREMENTS,
+)
