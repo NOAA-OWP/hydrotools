f1acfc09523ad408bac174359a1820abdba9447f
Austin Raney <arthur.raney@noaa.gov>
2020-12-14 17:27:16 -0500

    add crosswalk from usgs to nwm_feature_id

--- python/gcp_client/evaluation_tools/gcp_client/utils.py
+++ python/gcp_client/evaluation_tools/gcp_client/utils.py
@@ -65,3 +65,47 @@ def nwm_routelink_extract_usgs_sites(
 
     # return rows that have an associated gage
     return df[df["usgs_site_code"].str.len() > 0].reset_index(drop=True)
+
+
+def crosswalk(usgs_site_codes: Union[str, List[str]]) -> pd.DataFrame:
+    """Return the nwm_feature_id(s) for one or more provided usgs_sites_codes.
+
+    Parameters
+    ----------
+    usgs_site_codes : Union[str, List[str]]
+        USGS site code as string, string seperated by commas, or list of strings
+
+    Returns
+    -------
+    pd.DataFrame
+        df of `nwm_feature_id` and `usgs_site_code` cols
+
+    Examples
+    --------
+    >>> from evaluation_tools.gcp_client import utils
+    >>> cribbs_creek = "02465292"
+    >>> crx_walk = utils.crosswalk(cribbs_creek)
+
+    >>> sites = ["02465292", "04234000"]
+    >>> crx_walk = utils.crosswalk(sites)
+
+    >>> sites = "02465292,04234000"
+    >>> crx_walk = utils.crosswalk(sites)
+    """
+    # Path to crosswalk file
+    crosswalk_file = (
+        Path(__file__).resolve().parent / "data/nwm_2_0_feature_id_with_usgs_site.csv"
+    )
+
+    # Read crosswalk file in as df, ensure its the right data types
+    crosswalk_df = pd.read_csv(
+        crosswalk_file,
+        dtype={"nwm_feature_id": int, "usgs_site_code": str},
+    )
+
+    # If passed site codes are singular or in string form convert to list
+    if isinstance(usgs_site_codes, six.string_types):
+        usgs_site_codes = usgs_site_codes.split(",")
+
+    # Return df of nwm_feature_ids and matching usgs_site_codes
+    return crosswalk_df[crosswalk_df["usgs_site_code"].isin(usgs_site_codes)]
