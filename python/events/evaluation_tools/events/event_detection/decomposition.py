00f7035c4b2fdc222f098f6ec42e1ce2eba9e5d9
Jason Regina <jarq6c@gmail.com>
2021-01-26 14:29:00 -0600

    add type hints, residual assumptions, and explicit citation

--- python/events/evaluation_tools/events/event_detection/decomposition.py
+++ python/events/evaluation_tools/events/event_detection/decomposition.py
@@ -3,7 +3,11 @@
 Event Detection :: Decomposition
 ================================
 Use time series decomposition to detect hydroligical events in 
-streamflow time series.
+streamflow time series as described in Regina & Ogden, 2020.
+
+Regina, J.A., F.L. Ogden, 2020.  Automated Correction of Systematic 
+    Errors in High-Frequency Stage Data from V-Notch Weirs using 
+    Time Series Decomposition., Under review., Hydrol. Proc.
 
 Functions
 ---------
@@ -15,10 +19,19 @@ list_events
 
 import numpy as np
 import pandas as pd
+from typing import Union
 
-def detrend_streamflow(series, halflife, window, reverse=False) -> pd.Series:
+def detrend_streamflow(
+    series: pd.Series,
+    halflife: Union[float, str, pd.Timedelta],
+    window: Union[int, pd.offsets, pd.Index],
+    reverse: bool =False
+    ) -> pd.Series:
     """Model the trend in a streamflow time series using a rolling 
-        minimum filter. Remove the trend and residual components.
+        minimum filter. Remove the trend and residual components. The method 
+        aims to produce a detrended time series with a median of 0.0. It assumes 
+        any residual components less than twice the detrended median are random 
+        noise.
         Return the detrended streamflow time series.
         
         Parameters
@@ -74,10 +87,17 @@ def detrend_streamflow(series, halflife, window, reverse=False) -> pd.Series:
     # Return detrended series
     return detrended
 
-def mark_event_flows(series, halflife, window) -> pd.Series:
+def mark_event_flows(
+    series: pd.Series,
+    halflife: Union[float, str, pd.Timedelta],
+    window: Union[int, pd.offsets, pd.Index]
+    ) -> pd.Series:
     """Model the trend in a streamflow time series by taking the mean
         of two rolling minimum filters applied in a forward and 
-        backward fashion. Remove the trend and residual components.
+        backward fashion. Remove the trend and residual components. The method 
+        aims to produce a detrended time series with a median of 0.0. It assumes 
+        any residual components less than twice the detrended median are random 
+        noise.
         Return the boolean time series that indicates whether an 
         individual value in the original streamflow time series
         is part of an event (True) or not part of an event (False).
@@ -122,7 +142,11 @@ def mark_event_flows(series, halflife, window) -> pd.Series:
     # Return mask of non-zero detrended flows
     return pd.Series((detrended > 0.0), index=series.index)
 
-def list_events(series, halflife, window) -> pd.DataFrame:
+def list_events(
+    series: pd.Series,
+    halflife: Union[float, str, pd.Timedelta],
+    window: Union[int, pd.offsets, pd.Index]
+    ) -> pd.DataFrame:
     """Apply time series decomposition to mark event values in a streamflow
         time series. Discretize continuous event values into indiviual events.
         Return a DataFrame with one row for each event detected with `start` and 
