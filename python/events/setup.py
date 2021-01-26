76243d40f4aa697ee776d291ed36d9930f66b08e
Jason Regina <jarq6c@gmail.com>
2021-01-26 13:11:10 -0600

    initial commit containing event detection

---
+++ python/events/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_namespace_packages
+
+# python namespace subpackage
+# this namespace package follows PEP420
+# https://packaging.python.org/guides/packaging-namespace-packages/#native-namespace-packages
+
+NAMESPACE_PACKAGE_NAME = "evaluation_tools"
+SUBPACKAGE_NAME = "events"
+
+# Namespace subpackage slug.
+# Ex: mypkg.a # Where the namespace pkg = `mypkg` and the subpackage = `a`
+SUBPACKAGE_SLUG = f"{NAMESPACE_PACKAGE_NAME}.{SUBPACKAGE_NAME}"
+
+# Subpackage version
+VERSION = "0.1.0"
+
+# Package author information
+AUTHOR = "Jason Regina"
+AUTHOR_EMAIL = "jason.regina@noaa.gov"
+
+# Short sub-package description
+DESCRIPTION = "Various methods to support event-based evaluations."
+
+# Package dependency requirements
+REQUIREMENTS = [
+    "pandas",
+]
+
+setup(
+    name=SUBPACKAGE_SLUG,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    classifiers=[
+        "",
+    ],
+    description=DESCRIPTION,
+    namespace_packages=[NAMESPACE_PACKAGE_NAME],
+    packages=find_namespace_packages(include=[f"{NAMESPACE_PACKAGE_NAME}.*"]),
+    package_data={},
+    install_requires=REQUIREMENTS,
+)
