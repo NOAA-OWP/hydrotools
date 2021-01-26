42f46aae2fa13dff37d471c7f6c767073d0b5f7e
Jason Regina <jarq6c@gmail.com>
2021-01-26 13:11:23 -0600

    add events subpackage

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ AUTHOR = "Jason Regina"
 AUTHOR_EMAIL = "jarq6c@gmail.com"
 
 # Namespace package version
-VERSION = "1.0.0"
+VERSION = "1.2.0"
 URL = "https://github.com/NOAA-OWP/evaluation_tools"
 
 # Map subpackage namespace to relative location
@@ -31,6 +31,7 @@ SUBPACKAGES = {
     "evaluation_tools.nwis_client": "python/nwis_client",
     "evaluation_tools._restclient": "python/_restclient",
     "evaluation_tools.gcp_client": "python/gcp_client",
+    "evaluation_tools.events": "python/events",
 }
 
 # Short sub-package description
