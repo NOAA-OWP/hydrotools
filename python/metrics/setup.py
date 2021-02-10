45ccff9a15489b70501443df4c025630b463e719
jason.regina <jason.regina@noaa.gov>
2021-02-10 17:12:10 +0000

    advance version

--- python/metrics/setup.py
+++ python/metrics/setup.py
@@ -13,7 +13,7 @@ SUBPACKAGE_NAME = "metrics"
 SUBPACKAGE_SLUG = f"{NAMESPACE_PACKAGE_NAME}.{SUBPACKAGE_NAME}"
 
 # Subpackage version
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 # Package author information
 AUTHOR = "Jason Regina"
