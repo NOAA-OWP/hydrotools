3720f44cb63708c01d0b986a2b615d39406a0125
Austin Raney <aaraney@crimson.ua.edu>
2021-04-01 17:42:44 -0400

    update link to included package_data in gcp_client

--- python/gcp_client/setup.py
+++ python/gcp_client/setup.py
@@ -37,7 +37,7 @@ setup(
     namespace_packages=[NAMESPACE_PACKAGE_NAME],
     packages=find_namespace_packages(include=[f"{NAMESPACE_PACKAGE_NAME}.*"]),
     package_data={
-        "evaluation_tools.gcp_client": [
+        "hydrotools.gcp_client": [
             "data/nwm_2_0_feature_id_with_usgs_site.csv",
         ]
     },
