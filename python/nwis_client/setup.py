9779ab2dcf0bb6fccf237066cb9fcc573352a5fe
Austin Raney <aaraney@crimson.ua.edu>
2021-04-01 18:21:17 -0400

    Hotfix to fix tests breaking. The ref to the location of _restclient WILL NEED TO CHANGE before merging with main

--- python/nwis_client/setup.py
+++ python/nwis_client/setup.py
@@ -28,7 +28,8 @@ REQUIREMENTS = [
     "numpy",
     "requests",
     "requests-cache",
-    "hydrotools._restclient@ git+https://github.com/NOAA-OWP/hydrotools.git#subdirectory=python/_restclient",
+    # TODO: Temp fix for tests. This will not work once the branch is deleted
+    "hydrotools._restclient@ git+https://github.com/NOAA-OWP/hydrotools.git@move-namespace-to-hydrotools#subdirectory=python/_restclient",
 ]
 
 setup(
