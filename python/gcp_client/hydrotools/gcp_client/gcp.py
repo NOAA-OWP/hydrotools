03641601ffc2e931f0db0e11360a03dfb7963688
Jason Regina <jarq6c@gmail.com>
2021-04-12 15:19:11 -0500

    validate configuration

--- python/gcp_client/hydrotools/gcp_client/gcp.py
+++ python/gcp_client/hydrotools/gcp_client/gcp.py
@@ -31,6 +31,28 @@ _FEATURE_ID_TO_USGS_SITE_MAP_FILE = (
     Path(__file__).resolve().parent / "data/nwm_2_0_feature_id_with_usgs_site.csv"
 )
 
+# Valid configurations compatible with this client
+# TODO Find crosswalk for Hawaii, Puerto Rico, and US Virgin Islands
+_valid_configurations = [
+    'analysis_assim',
+    'analysis_assim_extend',
+    # 'analysis_assim_hawaii',
+    'analysis_assim_long',
+    'long_range_mem1',
+    'long_range_mem2',
+    'long_range_mem3',
+    'long_range_mem4',
+    'medium_range_mem1',
+    'medium_range_mem2',
+    'medium_range_mem3',
+    'medium_range_mem4',
+    'medium_range_mem5',
+    'medium_range_mem6',
+    'medium_range_mem7',
+    'short_range',
+    # 'short_range_hawaii'
+    ]
+
 class NWMDataService:
     """A Google Cloud Storage client class.
     The NWMDataService class provides various methods for constructing 
@@ -119,6 +141,11 @@ class NWMDataService:
         ...     )
         
         """
+        # Validate configuration
+        if configuration not in _valid_configurations:
+            message = f'Invalid configuration. Must select from {str(_valid_configurations)}'
+            raise ValueError(message)
+
         # Break-up reference time
         tokens = reference_time.split('T')
         issue_date = tokens[0]
