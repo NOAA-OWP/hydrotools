9d0d21c29ceb859e175a8b154875a6d3da79b8ed
Jason Regina <jarq6c@gmail.com>
2021-04-12 13:43:30 -0500

    test crosswalk

--- python/gcp_client/tests/test_gcp.py
+++ python/gcp_client/tests/test_gcp.py
@@ -15,20 +15,24 @@ def test_max_processes(setup_gcp):
     count = cpu_count() - 2
     assert (setup_gcp.max_processes) == count
 
-# @pytest.mark.slow
-# def test_list_blobs(setup_gcp):
-#     blob_list = setup_gcp.list_blobs(
-#         configuration="short_range",
-#         reference_time="20210101T01Z"
-#     )
+def test_crosswalk(setup_gcp):
+    assert setup_gcp.crosswalk['usgs_site_code'].count() > 4000
+    assert setup_gcp.crosswalk['usgs_site_code'].count() < 8000
 
-#     assert len(blob_list) == 18
+@pytest.mark.slow
+def test_list_blobs(setup_gcp):
+    blob_list = setup_gcp.list_blobs(
+        configuration="short_range",
+        reference_time="20210101T01Z"
+    )
 
-# @pytest.mark.slow
-# def test_get_blob(setup_gcp):
-#     blob_name = "nwm.20210101/short_range/nwm.t01z.short_range.channel_rt.f001.conus.nc"
-#     blob_data = setup_gcp.get_blob(blob_name)
-#     assert type(blob_data) == bytes
+    assert len(blob_list) == 18
+
+@pytest.mark.slow
+def test_get_blob(setup_gcp):
+    blob_name = "nwm.20210101/short_range/nwm.t01z.short_range.channel_rt.f001.conus.nc"
+    blob_data = setup_gcp.get_blob(blob_name)
+    assert type(blob_data) == bytes
 
 @pytest.mark.slow
 def test_get_Dataset(setup_gcp):
