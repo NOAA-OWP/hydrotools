3ed8ef418fb8aeced87096c6d158f617e357da96
Austin Raney <arthur.raney@noaa.gov>
2021-04-27 12:32:17 -0400

    tests for AliasGroup building obj multiple ways and edge cases

--- python/_restclient/tests/test_utilities.py
+++ python/_restclient/tests/test_utilities.py
@@ -66,28 +66,38 @@ def test__getitem__raises_value_error(alias_fixture):
         alias_fixture["None"]
 
 
-def test_alias_group():
+@pytest.fixture
+def true_false_AG_constructor():
     true = utilities.Alias(True, [True, 1, "true"])
     false = utilities.Alias(False, [False, 0, "false"])
 
-    assert true.get(True) is True
-
+    # ensure instantiation works in both cases
     group = utilities.AliasGroup([true, false])
-    assert group[True] is True
-    assert group[1] is True
-    assert group["true"] is True
+    assert isinstance(group, utilities.AliasGroup)
+    return group
 
-    assert group.get(True) is True
-    assert group.get(1) is True
-    assert group.get("true") is True
 
-    assert group[False] is False
-    assert group[0] is False
-    assert group["false"] is False
+@pytest.fixture
+def true_false_AG_using_or():
+    true = utilities.Alias(True, [True, 1, "true"])
+    false = utilities.Alias(False, [False, 0, "false"])
 
-    assert group.get(False) is False
-    assert group.get(0) is False
-    assert group.get("false") is False
+    group = true | false
+    assert isinstance(group, utilities.AliasGroup)
+    return group
+
+
+@pytest.fixture(
+    params=["true_false_AG_constructor", "true_false_AG_using_or"], name="group"
+)
+def true_false_AG_meta_fixture(request):
+    """Meta fixture returning multiple AG fixtures.  This is like
+    pytest.mark.parametrize, but with fixtures.
+    """
+    # request is a special pytest fixture that returns context information
+    # here, the `getfixturevalue` func is used to get the return value for each
+    # fixture passed in `params`
+    return request.getfixturevalue(request.param)
 
 
 def test_if_all_items_in_AliasGroup_not_Alias_should_ValueError():
@@ -103,13 +113,38 @@ def test_raise_ValueError_if_non_scalar_or_callable_in_Alias_constructor():
         utilities.Alias(["should-fail"], "please-fail")
 
 
-def test_build_AliasGroup_with_or():
-    true = utilities.Alias(True, [True, 1, "true"])
-    false = utilities.Alias(False, [False, 0, "false"])
+def test_AliasGroup_contains(group):
 
-    group = true | false
+    assert True in group
+    assert False in group
 
-    assert isinstance(group, utilities.AliasGroup)
+    assert "true" in group
+    assert "false" in group
+
+    assert 1 in group
+    assert 0 in group
+
+
+def test_AliasGroup__getitem__(group):
+
+    assert group["true"] is True
+    assert group[True] is True
+    assert group[1] is True
+
+    assert group["false"] is False
+    assert group[False] is False
+    assert group[0] is False
+
+
+def test_AliasGroup_get(group):
+
+    assert group.get("true") is True
+    assert group.get(True) is True
+    assert group.get(1) is True
+
+    assert group.get("false") is False
+    assert group.get(False) is False
+    assert group.get(0) is False
 
 
 def test_build_AliasGroup_with_or_fail_wrong_type():
@@ -117,4 +152,67 @@ def test_build_AliasGroup_with_or_fail_wrong_type():
     false = False
 
     with pytest.raises(TypeError):
-        group = true | false
+        true | false
+
+
+def test_Alias_contains():
+    true = utilities.Alias(True, "1")
+    true_2 = utilities.Alias(True, "2")
+    func = utilities.Alias(lambda x: x ** 2, "square")
+
+    assert "1" in true
+    assert True in true
+    assert true in true
+
+    assert "square" in func
+    assert func in func
+    x = lambda x: x ** 2
+    assert (x in func) == False
+
+    assert true_2 in true
+    assert true in true_2
+
+    assert (False in true) == False
+
+
+def test_reinstantiate_Alias_with_Alias_instance():
+    a = utilities.Alias(True, [1, 3])
+    assert a.value == True
+    assert 2 not in a.keys
+
+    b = utilities.Alias(a, [2])
+    assert b.value == True
+    assert 2 in b.keys
+
+
+def test_or_from_same_alias_value():
+    a = utilities.Alias(True, [1, 3])
+    b = utilities.Alias(True, [2, 2, 1])
+    c = a | b
+
+    assert isinstance(c, utilities.Alias)
+    assert 3 not in b
+    assert 2 not in a
+
+    assert 1 in c
+    assert 2 in c
+    assert 3 in c
+
+
+def test_build_AliasGroup_from_same_Alias(group):
+    a = utilities.Alias(False, ["False"])
+
+    # build AliasGroup from AliasGroup and Alias
+    c = group | a
+
+    assert isinstance(c, utilities.AliasGroup)
+    assert "False" in c
+    assert "False" not in group
+
+
+def test_AliasGroup_keys_prop():
+    a = utilities.Alias(True, [1, 3])
+    b = utilities.Alias(False, [4])
+    c = a | b  # AliasGroup
+
+    assert {1, 3, 4}.isdisjoint(c.keys) is False
