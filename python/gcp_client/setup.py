d0ea70a3b498c77ab882f485748bdc6160f0dfcd
jason.regina <jason.regina@noaa.gov>
2021-05-03 17:36:23 +0000

    increment version

--- python/gcp_client/setup.py
+++ python/gcp_client/setup.py
@@ -13,7 +13,7 @@ SUBPACKAGE_NAME = "gcp_client"
 SUBPACKAGE_SLUG = f"{NAMESPACE_PACKAGE_NAME}.{SUBPACKAGE_NAME}"
 
 # Subpackage version
-VERSION = "2.1.2"
+VERSION = "2.1.3"
 
 # Package author information
 AUTHOR = "Jason Regina"
