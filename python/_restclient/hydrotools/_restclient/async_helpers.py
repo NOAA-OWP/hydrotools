0a21859b0d0b504882752930eea429381b73a345
Austin Raney <arthur.raney@noaa.gov>
2021-05-07 16:54:53 -0400

    wrap func in coro method added and docstrings for other methods

--- python/_restclient/hydrotools/_restclient/async_helpers.py
+++ python/_restclient/hydrotools/_restclient/async_helpers.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import asyncio
-from typing import Coroutine
+from typing import Callable, Coroutine
+from functools import partial, wraps
 
 
 __all__ = []
@@ -12,11 +13,30 @@ class AsyncToSerialHelper:
         self,
         loop: asyncio.AbstractEventLoop = None,
     ) -> None:
-        self._loop = loop
+        self._loop = loop  # type: asyncio.AbstractEventLoop
 
         # if loop not passed, get running or start new
         if loop is None:
             self._loop = asyncio.get_event_loop()
 
     def _add_to_loop(self, coro: Coroutine):
+        """ Add coro to event loop via run_until_complete """
         return self._loop.run_until_complete(coro)
+
+    def _wrap_func_in_coro(self, func: Callable, *args, **kwargs):
+        """ Create partial func; wrap and call partial in coro; return coro """
+        part = partial(func, *args, **kwargs)
+
+        async def wrap():
+            return part()
+
+        return wrap
+
+    def _wrap_coro_in_callable(self, coro: Coroutine) -> Callable:
+        """ Wrap coro in method (that accepts args, kwargs) which adds coro to event loop. """
+
+        @wraps(coro)
+        def wrap(*args, **kwargs):
+            return self._add_to_loop(coro(*args, **kwargs))
+
+        return wrap
