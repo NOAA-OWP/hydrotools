d06310a649dce0c3042790552bf4ccbc20212e74
Austin Raney <arthur.raney@noaa.gov>
2021-05-07 18:17:56 -0400

    Breaking changes: major refactor to RestClient.
    Rest client now wraps the functionality of _restclient.ClientSession
    coros with methods run in event loop. This provides an easy to use API
    which still benefits from the speed improvements of aiohttp. Speed
    increases are most apparent using the new `mget` method which fulfills
    a list of requests.

--- python/_restclient/hydrotools/_restclient/_restclient.py
+++ python/_restclient/hydrotools/_restclient/_restclient.py
@@ -1,62 +1,94 @@
-"""
-Generic REST Api client usable/inheritable for general usage and adaptable to more
-specific use cases.
+#!/usr/bin/env python3
 
-# TODO: Add more information about the class here
-"""
-
-from typing import Any, Dict, List, Tuple, Union
-from collections.abc import Iterable
+from typing import Any, List, Union
 import asyncio
 from aiohttp_client_cache import SQLiteBackend
-
-import requests
-import requests_cache
-from urllib3.util.retry import Retry
+import aiohttp
+import forge
+import atexit
 
 # local imports
 from .async_client import ClientSession
 from .async_helpers import AsyncToSerialHelper
+from .urllib import Url
+from ._restclient_sigs import GET_SIGNATURE, MGET_SIGNATURE
+
+__all__ = ["RestClient"]
 
 
 class RestClient(AsyncToSerialHelper):
-    """Provides various methods for constructing requests, retrieving data, and
-    parsing responses from the NWIS IV Service.
+    """
+    Class adept for supporting and simplifying RESTful client libraries and retrieval
+    scripts. Behind the scenes requests are made asynchronously, however the API is
+    exposed using serial methods to simplify usage.
+
+    Features
+    --------
+    - Base url
+    - SQLite request cache
+    - Retry exponential backoff
+    - Serial wrapper methods that make requests asynchronously (see `RestClient.mget`)
 
     Parameters
     ----------
-    processes : int
-        Max multiprocessing processes, default 3
-    retry : int
-        Max number of, per request retries, default 3
-    """
+    base_url: Union[str, Url, None], default None
+        Request base url
+    headers: dict, default {}
+        Headers included in every request
+    enable_cache: bool, default True
+        Enable or disable caching
+    requests_cache_filename: str, default "cache"
+        Cache filename with .sqlite filetype suffix
+    requests_cache_expire_after: int, default 43200
+        Cached request life in seconds
+    retry: bool, default True
+        Enable exponential backoff
+    n_retries: int, default 3
+        Attempt retries n times before failing
+    loop: asyncio.AbstractEventLoop, default None
+        Async event loop
 
-    _response_codes = {
-        200: "OK",
-        304: "Not_Modified",
-        400: "Bad_Request",
-        403: "Access_Forbidden",
-        404: "Not_Found",
-        500: "Internal_Server_Error",
-        503: "Service_Unavailable",
-    }
+    Examples
+    --------
+
+    Simple Request
+    >>> from hydrotools._restclient import RestClient
+    >>>
+    >>> client = RestClient()
+    >>> resp = client.get("weather.gov")
+    >>> print(resp.text())
+
+
+    USGS NLDI Requests
+    >>> from hydrotools._restclient import RestClient
+    >>>
+    >>> base_url = "https://labs.waterdata.usgs.gov/api/nldi/linked-data/nwissite/"
+    >>> headers = {"Accept": "*/*", "Accept-Encoding": "gzip, compress"}
+    >>> client = RestClient(base_url=base_url, headers=headers)
+    >>>
+    >>> sites = ["0423360405", "05106000","05078520","05078470","05125039","05124982"]
+    >>> site_urls = [f"/USGS-{site}/navigation/UT/flowlines" for site in sites]
+    >>> requests = client.mget(site_urls)
+    >>> requests = [resp.json() for resp in requests]
+    """
 
     def __init__(
         self,
         *,
-        base_url: Union[str, None] = None,
-        headers: Union[dict, None] = None,
+        base_url: Union[str, Url, None] = None,
+        headers: dict = {},
         enable_cache: bool = True,
         requests_cache_filename: str = "cache",
         requests_cache_expire_after: int = 43200,
         retry: bool = True,
-        retries: int = 3,
+        n_retries: int = 3,
         loop: asyncio.AbstractEventLoop = None,
     ):
-        super().__init__(loop)  # implicitly adds self._loop
-        self._base_url = base_url
+        # implicitly adds self._loop and gets loop if None provided
+        super().__init__(loop)
+        self._base_url = Url(base_url) if base_url is not None else None
         self._headers = headers
-        self._retires = retries
+        self._retires = n_retries
         self._cache_enabled = enable_cache
 
         cache = None
@@ -68,155 +100,112 @@ class RestClient(AsyncToSerialHelper):
                 allowed_methods=["GET"],
             )
 
+        # wrap ClientSession in coroutine and call in event loop
         self._session = self._add_to_loop(
-            ClientSession(cache=cache, retry=retry, n_retries=retries)
+            self._wrap_func_in_coro(
+                ClientSession, cache=cache, retry=retry, n_retries=retries
+            )()
+        )  # type: ClientSession
+
+        # register session removal at normal exit
+        atexit.register(self.release_session)
+
+    @GET_SIGNATURE
+    def get(self, url, *, parameters, headers, **kwargs):
+        """Make GET request. If base url is set, url is appended to the base url.
+        Likewise for request headers.
+
+        Parameters
+        ----------
+        url : str, Url
+            Request url
+        parameters : Dict[str, Union[str, List[str, int, float]]]
+            Query parameters
+        headers : Dict[str, str]
+            Request headers, if RestClient headers set provided headers are appended
+
+        Returns
+        -------
+        aiohttp.ClientResponse
+        """
+
+        return self._add_to_loop(
+            self._get(url, parameters=parameters, headers=headers, **kwargs)
         )
 
-    def get(
+    @MGET_SIGNATURE
+    def mget(self, urls, *, parameters, headers, **kwargs):
+        """Make multiple asynchronous GET request. If base url is set, each url is
+        appended to the base url.  Likewise for request headers.
+
+        Parameters
+        ----------
+        urls : List[Union[str, Url]]
+            Request urls
+        parameters : Dict[str, Union[str, List[str, int, float]]]
+            Query parameters
+        headers : Dict[str, str]
+            Request headers, if RestClient headers set provided headers are appended
+
+        Returns
+        -------
+        List[aiohttp.ClientResponse]
+        """
+        return self._add_to_loop(
+            self._mget(urls, parameters=parameters, headers=headers, **kwargs)
+        )
+
+    @GET_SIGNATURE
+    async def _get(
         self,
         url: str = None,
-        parameters: dict = None,
-        headers: dict = None,
-        parameter_delimeter: str = None,
+        *,
+        parameters,
+        headers,
         **kwargs,
-    ):
-        if not isinstance(url, str):
-            if not isinstance(self._base_url, str):
-                error_message = "A base_url was not set nor was a url passed."
-                raise AttributeError(error_message)
+    ) -> aiohttp.ClientResponse:
+        if self.base_url is not None:
+            url = self._base_url / url
 
-            url = self._base_url
+        # add query parameters and get quoted representation
+        url = (Url(url) + parameters).quote_url
 
-        request = self.Request(
-            url=url,
-            parameters=parameters,
-            headers=headers,
-            parameter_delimiter=parameter_delimeter,
-        )
-        return self.Get(request, **kwargs)
+        resp = await self._session.get(url, headers=headers, **kwargs)
 
-    def Request(
+        # implicitly sets resp._body which contains response data
+        await resp.read()
+
+        # release the resource
+        # note, resp is "locked" until released meaning it cannot be patched until post-release
+        resp.release()
+        # Patch and wrap resp's, text and json methods with run_until_complete and forge their signatures
+        resp = self._patch_get(resp)
+        return resp
+
+    @MGET_SIGNATURE
+    async def _mget(
         self,
-        url: str = None,
-        parameters: Dict[str, Union[str, List[Any]]] = None,
-        parameter_delimiter: Union[str, None] = None,
-        headers: Dict[str, str] = None,
-    ) -> requests.PreparedRequest:
-        """Create Prepared Request from url and parameters and headers. Parameter
-        concatenation behavior can be changed by passing a different
-        `parameter_delimiter` argument. For example, a comma could be specified for
-        USGS. (i.e. https://example-site.com/?var=1,2,3)
+        urls,
+        *args,
+        **kwargs: Any,
+    ) -> List[aiohttp.ClientResponse]:
+        return await asyncio.gather(*[self._get(url, *args, **kwargs) for url in urls])
 
-        Parameters
-        ----------
-        url : str, optional
-            url on which parameters are appended, by default self._base_url
-        parameters : Dict[str, Union[str, List[Any]], optional
-            parameters to append to url, by default None
-        parameter_delimiter : Union[str, None], optional
-            delimiter to separate parameters.
-            None will resume normal Requests behavior, by default ","
-        headers : Dict[str, str], optional
-            headers, by default None
+    def _patch_get(
+        self, client_response: aiohttp.ClientResponse
+    ) -> aiohttp.ClientResponse:
+        """ Wrap aiohttp.ClientResponse text and json coros in run_until_complete. Monkeypatch text and json with wrappers."""
+        # May iter through methods and wrap all coro's in the future
+        # however that may not work if a non-coro returns a async context manager for example
+        text = self._wrap_coro_in_callable(client_response.text)
+        json = self._wrap_coro_in_callable(client_response.json)
+        # resign signatures
+        text = forge.copy(aiohttp.ClientResponse.text, exclude="self")(text)
+        json = forge.copy(aiohttp.ClientResponse.json, exclude="self")(json)
 
-        Returns
-        -------
-        requests.PreparedRequest
-            Prepared requests object
-
-        Examples
-        --------
-        >>> 
-        """
-
-        # Handle default headers.
-        if not isinstance(headers, dict):
-            headers = {}
-
-        if parameters and parameter_delimiter:
-            # Join key parameters that are in a list with delimiter
-            # `parameter_delimiter`
-            for key, value in parameters.items():
-                if isinstance(value, Iterable) and not isinstance(value, str):
-                    try:
-                        parameters[key] = f"{parameter_delimiter}".join(value)
-                    except TypeError:
-                        value = map(str, value)
-                        parameters[key] = f"{parameter_delimiter}".join(value)
-
-        # Build GET request url
-        return requests.Request("GET", url, params=parameters).prepare()
-
-    def Get(self, request: requests.PreparedRequest, **kwargs,) -> requests.Response:
-        """ Thin request.Session.get wrapper. Take prepared request and get
-        response handling errors along the way. Only requests status codes
-        200 and 201 returned.If the initial request fails, `self._retries`
-        number retries are attempted with a 0.1 backoff factor.
-
-        Parameters
-        ----------
-        request : requests.PreparedRequest
-            Prepared request object. Header and url are included
-        kwargs :
-            Keyword arguments passed to requests.Session().send
-            See: https://requests.readthedocs.io/en/latest/_modules/requests/sessions/#Session.send
-            
-
-        Returns
-        -------
-        requests.Response
-            request.Session.get response.
-
-        Raises
-        ------
-        requests.exceptions.ConnectionError
-           Raise if receive non 200 or 201 response
-        """
-        try:
-            with self._create_session() as session:
-                response = session.send(request, **kwargs)
-
-                if response.status_code == 201 or response.status_code == 200:
-                    return response
-
-                # Failed to retrieve
-                error_message = (
-                    "Retrieval failed\n"
-                    + f"Server code: {response.status_code}\n"
-                    + f"Server status: {self._response_codes[response.status_code]}\n"
-                    + f"Query url:\n{request.url}"
-                )
-                raise requests.exceptions.ConnectionError(error_message)
-
-        except requests.ConnectionError:
-            error_message = f"Verify the {request.url} is correct"
-            BaseException(error_message)
-            raise
-
-    def _create_session(self):
-        """Create requests.Session object with concrete retry strategy.
-
-        Returns
-        -------
-        requests.Session
-            Session
-        """
-
-        with requests.Session() as session:
-
-            # retry times and backoff factor (how long to sleep in between
-            # retries)
-            retires = Retry(
-                total=self._retires,
-                backoff_factor=0.1,
-                status_forcelist=[500, 502, 503, 504],
-            )
-
-            session.mount(
-                "https://", requests.adapters.HTTPAdapter(max_retries=retires)
-            )
-
-            return session
+        client_response.text = text
+        client_response.json = json
+        return client_response
 
     @property
     def base_url(self) -> str:
@@ -228,26 +217,11 @@ class RestClient(AsyncToSerialHelper):
         """ GET request headers """
         return self._headers
 
+    def release_session(self) -> None:
+        """ Release aiohttp.ClientSession """
+        if not self._session.closed:
+            self._add_to_loop(self._session.close())
 
-class Variadic(str):
-    """Specify list or tuple as delimeted str. The is useful when specifying arguments
-    in a URL that do not conform to the &key=value&key=value2 convention.
-
-    Example:
-        url = "https://www.test.gov",
-        async with ClientSession() as session:
-            async with session.get(url,
-                params={"this": Variadic(["that", "the", "other"])},
-            ) as req:
-                print(req.url) # Returns, https://www.test.gov/?this=that,the,other
-
-    """
-
-    def __init__(self, values: Union[List, Tuple], *, delimeter: str = ",") -> None:
-        self.values = delimeter.join([str(v) for v in values])
-
-    def __repr__(self):
-        return self.values
-
-    def __str__(self) -> str:
-        return repr(self)
+    def __del__(self) -> None:
+        atexit.unregister(self.release_session)
+        self.release_session()
