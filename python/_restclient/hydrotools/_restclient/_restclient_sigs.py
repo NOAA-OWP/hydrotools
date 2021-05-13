0edeec8333893e1a121363be614b890ac2e7f308
Austin Raney <arthur.raney@noaa.gov>
2021-05-13 11:56:46 -0400

    refactor function signatures

--- python/_restclient/hydrotools/_restclient/_restclient_sigs.py
+++ python/_restclient/hydrotools/_restclient/_restclient_sigs.py
@@ -5,26 +5,37 @@ from typing import Dict, List, Union
 
 # RestClient signature decorators
 
-_PARAM_HEADER_KWOS = forge.insert(
-    [
-        forge.kwo("parameters", default={}, type=Dict[str, Union[str, List[str]]]),
-        forge.kwo("headers", default={}, type=Dict[str, str]),
-    ],
-    before=lambda arg: arg.kind == forge.FParameter.VAR_KEYWORD,
-)
-
 GET_SIGNATURE = forge.compose(
     forge.copy(aiohttp.ClientSession.get),
+    forge.modify("url", default=None),
     forge.returns(aiohttp.ClientResponse),
-    _PARAM_HEADER_KWOS,
+    forge.insert(
+        [
+            forge.kwo("parameters", default={}, type=Dict[str, Union[str, List[str]]]),
+            forge.kwo("headers", default={}, type=Dict[str, str]),
+        ],
+        before=lambda arg: arg.kind == forge.FParameter.VAR_KEYWORD,
+    ),
 )
 
 MGET_SIGNATURE = forge.compose(
     forge.copy(aiohttp.ClientSession.get, exclude="url"),
     forge.returns(List[aiohttp.ClientResponse]),
     forge.insert(
-        forge.pok("urls", type=forge.fsignature(aiohttp.ClientSession.get)["url"].type),
+        forge.pok(
+            "urls",
+            type=forge.fsignature(aiohttp.ClientSession.get)["url"].type,
+            default=None,
+        ),
         index=1,
     ),
-    _PARAM_HEADER_KWOS,
+    forge.insert(
+        [
+            forge.kwo(
+                "parameters", default={}, type=List[Dict[str, Union[str, List[str]]]]
+            ),
+            forge.kwo("headers", default={}, type=List[Dict[str, str]]),
+        ],
+        before=lambda arg: arg.kind == forge.FParameter.VAR_KEYWORD,
+    ),
 )
