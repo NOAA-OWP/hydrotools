3406c623f3d02de60f1ff60468a234b2aac9ca53
Austin Raney <arthur.raney@noaa.gov>
2021-06-03 16:05:05 -0500

    include Quote enum in _restclient __init__.py for import sake

--- python/_restclient/hydrotools/_restclient/__init__.py
+++ python/_restclient/hydrotools/_restclient/__init__.py
@@ -1,4 +1,5 @@
 from ._restclient import RestClient
 from .utilities import Alias, AliasGroup
 from .urllib import Url, Variadic
+from .urllib_types import Quote
 from .async_client import ClientSession
