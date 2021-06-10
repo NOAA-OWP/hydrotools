6eff21342ff5fc8fce4047f997ff8d14ecaa4b96
Austin Raney <arthur.raney@noaa.gov>
2021-06-10 15:01:03 -0400

    add required nbsphinx_multilink extension for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ release = '2.0.0-alpha.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        "sphinx.ext.autodoc", "sphinx.ext.napoleon"
+        "sphinx.ext.autodoc", "sphinx.ext.napoleon", "nbsphinx_multilink"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
