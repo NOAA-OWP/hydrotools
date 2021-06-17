ef2330b00132fb0ed83bd4c43629aaa33e3b3e77
Jason Regina <jarq6c@gmail.com>
2021-06-17 15:14:24 -0500

    add long description and classifiers

--- python/_restclient/setup.py
+++ python/_restclient/setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_namespace_packages
+from pathlib import Path
 
 # python namespace subpackage
 # this namespace package follows PEP420
@@ -13,7 +14,7 @@ SUBPACKAGE_NAME = "_restclient"
 SUBPACKAGE_SLUG = f"{NAMESPACE_PACKAGE_NAME}.{SUBPACKAGE_NAME}"
 
 # Subpackage version
-VERSION = "3.0.0"
+VERSION = "3.0.1"
 
 # Package author information
 AUTHOR = "Austin Raney"
@@ -21,6 +22,10 @@ AUTHOR_EMAIL = "arthur.raney@noaa.gov"
 
 # Short sub-package description
 DESCRIPTION = "General REST api client with built in request caching and retries."
+
+# Long description
+with (Path(__file__).parent / "README.md").open('r') as f:
+    LONG_DESCRIPTION = f.read()
 
 # Package dependency requirements
 REQUIREMENTS = ["aiohttp", "aiohttp_client_cache", "python-forge", "aiosqlite"]
@@ -32,8 +37,17 @@ setup(
     version=VERSION,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    classifiers=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: Free To Use But Restricted",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Hydrology"
+    ],
     description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     url="https://www.github.com/NOAA-OWP/hydrotools",
     license="USDOC",
     namespace_packages=[NAMESPACE_PACKAGE_NAME],
