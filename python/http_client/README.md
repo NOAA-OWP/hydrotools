b8e67fc117553d8033e6797ef1f4d997839702ff
Jason Regina <jarq6c@gmail.com>
2021-08-02 15:24:18 -0500

    mock up

---
+++ python/http_client/README.md
@@ -0,0 +1,97 @@
+# HydroTools :: HTTP Client
+
+This subpackage implements an interface to retrieve National Water Model (NWM) data from a generic http web server. The primary use for this tool is to populate `pandas.Dataframe` objects with NWM streamflow data. See the [HTTP Client Documentation](https://noaa-owp.github.io/hydrotools/hydrotools.http_client.html) for a complete list and description of the currently available methods. To report bugs or request new features, submit an issue through the [HydroTools Issue Tracker](https://github.com/NOAA-OWP/hydrotools/issues) on GitHub.
+
+## Installation
+
+In accordance with the python community, we support and advise the usage of virtual
+environments in any workflow using python. In the following installation guide, we
+use python's built-in `venv` module to create a virtual environment in which the
+tool will be installed. Note this is just personal preference, any python virtual
+environment manager should work just fine (`conda`, `pipenv`, etc. ).
+
+```bash
+# Create and activate python environment, requires python >= 3.8
+$ python3 -m venv env
+$ source env/bin/activate
+$ python3 -m pip install --upgrade pip wheel
+
+# Install HTTP_client
+$ python3 -m pip install hydrotools.http_client
+```
+
+## Usage
+
+The following example demonstrates how one might use `hydrotools.http_client` to retrieve NWM streamflow forecasts.
+
+### Code
+```python
+# Import the HTTP Client
+from hydrotools.http_client import nwm
+
+# Instantiate model data service
+model_data_service = nwm.NWMDataService(
+    server="http://0.0.0.0:9000/",
+    verify=None
+    )
+
+# Retrieve forecast data
+#  By default, only retrieves data at USGS gaging sites in
+#  CONUS that are used for model assimilation
+forecast_data = model_data_service.get(
+    configuration = "short_range",
+    reference_time = "20210101T01Z"
+    )
+
+# Look at the data
+print(forecast_data.info(memory_usage='deep'))
+print(forecast_data[['value_time', 'value']].head())
+```
+### Output
+```console
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 137628 entries, 0 to 137627
+Data columns (total 8 columns):
+ #   Column            Non-Null Count   Dtype         
+---  ------            --------------   -----         
+ 0   reference_time    137628 non-null  datetime64[ns]
+ 1   value_time        137628 non-null  datetime64[ns]
+ 2   nwm_feature_id    137628 non-null  int64         
+ 3   value             137628 non-null  float32       
+ 4   usgs_site_code    137628 non-null  category      
+ 5   configuration     137628 non-null  category      
+ 6   measurement_unit  137628 non-null  category      
+ 7   variable_name     137628 non-null  category      
+dtypes: category(4), datetime64[ns](2), float32(1), int64(1)
+memory usage: 5.1 MB
+None
+           value_time  value
+0 2021-01-01 02:00:00   5.29
+1 2021-01-01 03:00:00   5.25
+2 2021-01-01 04:00:00   5.20
+3 2021-01-01 05:00:00   5.12
+4 2021-01-01 06:00:00   5.03
+```
+### System Requirements
+We employ several methods to make sure the resulting `pandas.DataFrame` produced by `http_client` are as efficient and manageable as possible. Nonetheless, this package can potentially use a large amount of memory.
+
+The National Water Model generates multiple forecasts per day at over 3.7 million locations across the United States. A single forecast could be spread across hundreds of files and require repeated calls to Google Cloud Platform. The intermediate steps of retrieving and processing these files into leaner `DataFrame` may use several GB of memory. As such, recommended minimum requirements to use this package are a 4-core consumer processor and 8 GB of RAM.
+
+## Development
+
+This package uses a setup configuration file (`setup.cfg`) and assumes use of the `setuptools` backend to build the package. To install the package for development use:
+```bash
+$ python3 -m venv env
+$ source env/bin/activate
+$ python3 -m pip install -U pip
+$ python3 -m pip install -U setuptools
+$ python3 -m pip install -e .[develop]
+```
+
+To generate a source distribution:
+```bash
+$ python3 -m pip install -U wheel build
+$ python3 -m build
+```
+
+The packages generated in `dist/` can be installed directly with `pip` or uploaded to PyPI using `twine`.
---
+++ python/http_client/tests/test_http_nwm.py
@@ -0,0 +1,232 @@
+import pytest
+from hydrotools.gcp_client import gcp
+from os import cpu_count
+import numpy as np
+import pandas as pd
+
+@pytest.fixture
+def setup_gcp():
+    return gcp.NWMDataService()
+
+def test_bucket_name(setup_gcp):
+    assert (setup_gcp.bucket_name) == "national-water-model"
+
+def test_max_processes(setup_gcp):
+    count = max(cpu_count() - 2, 1)
+    assert (setup_gcp.max_processes) == count
+
+def test_crosswalk(setup_gcp):
+    assert setup_gcp.crosswalk['usgs_site_code'].count() > 4000
+    assert setup_gcp.crosswalk['usgs_site_code'].count() < 9000
+
+    with pytest.raises(Exception):
+        setup_gcp.crosswalk = 0
+
+    with pytest.raises(Exception):
+        setup_gcp.crosswalk = pd.DataFrame()
+
+    assert setup_gcp.crosswalk.loc[6186112, "usgs_site_code"] == "01360640"
+
+def test_cache_path(setup_gcp):
+    assert str(setup_gcp.cache_path) == 'gcp_client.h5'
+
+    setup_gcp.cache_path = 'custom_cache.h5'
+    assert str(setup_gcp.cache_path) == 'custom_cache.h5'
+
+def test_cache_group(setup_gcp):
+    assert str(setup_gcp.cache_group) == 'gcp_client'
+
+    setup_gcp.cache_group = 'simulations'
+    assert str(setup_gcp.cache_group) == 'simulations'
+
+@pytest.mark.slow
+def test_list_blobs(setup_gcp):
+    blob_list = setup_gcp.list_blobs(
+        configuration="short_range",
+        reference_time="20210101T01Z"
+    )
+
+    assert len(blob_list) == 18
+
+@pytest.mark.slow
+def test_get_blob(setup_gcp):
+    blob_name = "nwm.20210101/short_range/nwm.t01z.short_range.channel_rt.f001.conus.nc"
+    blob_data = setup_gcp.get_blob(blob_name)
+    assert type(blob_data) == bytes
+
+@pytest.mark.slow
+def test_get_Dataset(setup_gcp):
+    blob_name = "nwm.20210101/short_range/nwm.t01z.short_range.channel_rt.f001.conus.nc"
+    
+    # Test default filter
+    ds = setup_gcp.get_Dataset(blob_name)
+    assert ds.feature_id.size > 4000
+    assert ds.feature_id.size < 8000
+
+    # Test filter with list
+    ds = setup_gcp.get_Dataset(blob_name, 
+        feature_id_filter=[101, 179])
+    assert ds.feature_id.size == 2
+
+    # Test filter with numpy.array
+    ds = setup_gcp.get_Dataset(blob_name, 
+        feature_id_filter=np.asarray([101, 179]))
+    assert ds.feature_id.size == 2
+
+    # Test filter with pandas.Series
+    features = pd.Series(data=[101, 179])
+    ds = setup_gcp.get_Dataset(blob_name, 
+        feature_id_filter=features)
+    assert ds.feature_id.size == 2
+
+    # Test no filter
+    ds = setup_gcp.get_Dataset(blob_name, 
+        feature_id_filter=False)
+    assert ds.feature_id.size > 8000
+
+@pytest.mark.slow
+def test_get_DataFrame(setup_gcp):
+    blob_name = "nwm.20210101/short_range/nwm.t01z.short_range.channel_rt.f001.conus.nc"
+    
+    # Test default
+    df = setup_gcp.get_DataFrame(blob_name)
+    assert len(df.columns) == 4
+
+    # Test all variables
+    df = setup_gcp.get_DataFrame(blob_name, streamflow_only=False)
+    assert len(df.columns) > 4
+
+@pytest.mark.slow
+def test_get_cycle(setup_gcp):
+    # Test ANA
+    df = setup_gcp.get_cycle(
+        configuration="analysis_assim",
+        reference_time="20210101T01Z"
+    )
+    assert df['value_time'].unique().size == 3
+
+    # Test mapping
+    df = df[df["usgs_site_code"] == "01360640"]
+    assert df["nwm_feature_id"].iloc[0] == 6186112
+
+@pytest.mark.slow
+def test_cache_disable(setup_gcp):
+    setup_gcp.cache_path = 'disabled_cache.h5'
+
+    # Test ANA
+    df = setup_gcp.get(
+        configuration="analysis_assim",
+        reference_time="20210101T01Z",
+        cache_data=False
+    )
+    assert df['value_time'].unique().size == 3
+    assert not setup_gcp.cache_path.exists()
+
+@pytest.mark.slow
+def test_cache_key(setup_gcp):
+    # Test ANA
+    df1 = setup_gcp.get(
+        configuration="analysis_assim",
+        reference_time="20210101T02Z",
+        cache_data=True
+    )
+    first = df1['value_time'].unique().size
+
+    with pd.HDFStore(setup_gcp.cache_path) as store:
+        df2 = store[f"/{setup_gcp.cache_group}/analysis_assim/DT20210101T02Z"]
+        second = df2['value_time'].unique().size
+    assert first == second
+
+@pytest.mark.slow
+def test_get(setup_gcp):
+    # Test ANA
+    df = setup_gcp.get(
+        configuration="analysis_assim",
+        reference_time="20210101T01Z"
+    )
+    assert df['value_time'].unique().size == 3
+
+    # Test Ext. ANA
+    df = setup_gcp.get(
+        configuration="analysis_assim_extend",
+        reference_time="20210101T16Z"
+    )
+    assert df['value_time'].unique().size == 28
+
+    # Test short range
+    df = setup_gcp.get(
+        configuration="short_range",
+        reference_time="20210101T01Z"
+    )
+    assert df['value_time'].unique().size == 18
+
+    # Test medium range
+    df = setup_gcp.get(
+        configuration="medium_range_mem1",
+        reference_time="20210101T06Z"
+    )
+    assert df['value_time'].unique().size == 80
+
+    # Test long range
+    df = setup_gcp.get(
+        configuration="long_range_mem1",
+        reference_time="20210101T06Z"
+    )
+    assert df['value_time'].unique().size == 120
+
+    # Test Hawaii ANA
+    df = setup_gcp.get(
+        configuration="analysis_assim_hawaii",
+        reference_time="20210101T01Z"
+    )
+    assert df['value_time'].unique().size == 3
+
+    # Test Hawaii Short Range
+    df = setup_gcp.get(
+        configuration="short_range_hawaii",
+        reference_time="20210101T00Z"
+    )
+    assert df['value_time'].unique().size == 60
+
+    # Test Puerto Rico ANA
+    df = setup_gcp.get(
+        configuration="analysis_assim_puertorico",
+        reference_time="20210501T00Z"
+    )
+    assert df['value_time'].unique().size == 3
+
+    # Test Puerto Rico Short Range
+    df = setup_gcp.get(
+        configuration="short_range_puertorico",
+        reference_time="20210501T06Z"
+    )
+    assert df['value_time'].unique().size == 48
+
+@pytest.mark.slow
+def test_get_no_da(setup_gcp):
+    # No DA Cycles
+    cycles = [
+        ('analysis_assim_no_da', "20210501T06Z", 3),
+        ('analysis_assim_extend_no_da', "20210501T16Z", 28),
+        ('analysis_assim_hawaii_no_da', "20210501T00Z", 12),
+        ('analysis_assim_long_no_da', "20210501T00Z", 12),
+        ('analysis_assim_puertorico_no_da', "20210501T00Z", 3),
+        ('medium_range_no_da', "20210501T00Z", 80),
+        ('short_range_hawaii_no_da', "20210501T00Z", 192),
+        ('short_range_puertorico_no_da', "20210501T06Z", 48)
+    ]
+    # Test
+    for cycle, ref_tm, validate in cycles:
+        df = setup_gcp.get(
+            configuration=cycle,
+            reference_time=ref_tm
+        )
+        assert df['value_time'].unique().size == validate
+
+def test_invalid_configuration_exception(setup_gcp):
+    # Test invalid configuration
+    with pytest.raises(Exception):
+        df = setup_gcp.get(
+            configuration="medium_range",
+            reference_time="20210101T06Z"
+        )
