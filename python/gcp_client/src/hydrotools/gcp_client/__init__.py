da1d260315c21440d37bf54eb638de7066add5cd
Jason Regina <jarq6c@gmail.com>
2021-08-03 11:56:50 -0500

    deprecate gcp client

--- python/gcp_client/src/hydrotools/gcp_client/__init__.py
+++ python/gcp_client/src/hydrotools/gcp_client/__init__.py
@@ -1,1 +1,5 @@
 from .gcp import NWMDataService
+import warnings
+
+warning_message = "Importing gcp_client is deprecated. Import hydrotools.nwm_client.gcp"
+warnings.warn(warning_message, UserWarning)
