e6d381d3d8987daaa64f40d3a5a624dab5b443e0
Jason Regina <jarq6c@gmail.com>
2021-09-10 11:46:39 -0500

    test ssl handling

--- python/nwm_client/workbench/main.py
+++ python/nwm_client/workbench/main.py
@@ -1,21 +1,31 @@
-from nwm_client.FileDownloader import FileDownloader
-from nwm_client.NWMDataProcessor import NWMDataProcessor as NWM
-from pathlib import Path
-import xarray as xr
+# from nwm_client.FileDownloader import FileDownloader
+# from nwm_client.NWMDataProcessor import NWMDataProcessor as NWM
+# from pathlib import Path
+# import xarray as xr
+from nwm_client.NWMDataSource import HTTPFileSource
+import pandas as pd
 
 def main():
-    url1 = "https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod/nwm.20210909/analysis_assim/nwm.t00z.analysis_assim.channel_rt.tm00.conus.nc"
-    url2 = "https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod/nwm.20210909/analysis_assim/nwm.t00z.analysis_assim.channel_rt.tm01.conus.nc"
-    urls = [url1, url2]
+    # url1 = "https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod/nwm.20210909/analysis_assim/nwm.t00z.analysis_assim.channel_rt.tm00.conus.nc"
+    # url2 = "https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod/nwm.20210909/analysis_assim/nwm.t00z.analysis_assim.channel_rt.tm01.conus.nc"
+    # urls = [url1, url2]
 
-    odir = Path("./output")
+    # odir = Path("./output")
     # downloader = FileDownloader(odir, True)
     # downloader.get(urls)
 
     # Process data
-    df = NWM.get_dataframe(odir)
-    print(df.info(memory_usage="deep"))
-    print(df)
+    # df = NWM.get_dataframe(odir)
+    # print(df.info(memory_usage="deep"))
+    # print(df)
+
+    source = HTTPFileSource(
+        server="https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod/"
+        )
+    print(source.list_blobs(
+        configuration="short_range",
+        reference_time="20210910T00Z"
+    ))
 
 if __name__ == "__main__":
     main()
