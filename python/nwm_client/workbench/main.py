07290c0eba469e5a2c24bf11598723e6b09de2cb
Jason Regina <jarq6c@gmail.com>
2021-09-10 15:36:05 -0500

    caching test

--- python/nwm_client/workbench/main.py
+++ python/nwm_client/workbench/main.py
@@ -3,32 +3,47 @@ from nwm_client.NWMFileProcessor import NWMFileProcessor
 from nwm_client.NWMFileCatalog import HTTPFileCatalog
 
 import tempfile
+from pathlib import Path
+import dask.dataframe as dd
 
 def main():
     verify = None
-    # Get list of files
-    catalog = HTTPFileCatalog(
-        server="https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod/",
-        # verify=verify
+    configuration = "short_range"
+    reference_time = "20210910T00Z"
+    server = "https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod/"
+
+    # Check local file cache
+    local_file = Path(f"nwm_cache.parquet/{configuration}/RT{reference_time}")
+    if local_file.exists():
+        df = dd.read_parquet(local_file, write_index=False, compression="snappy")
+    else:
+        # Get list of files
+        catalog = HTTPFileCatalog(
+            server=server,
+            verify=verify
+            )
+        urls = catalog.list_blobs(
+            configuration=configuration,
+            reference_time=reference_time
         )
-    urls = catalog.list_blobs(
-        configuration="analysis_assim",
-        reference_time="20210910T00Z"
-    )
 
-    # Download and process files
-    with tempfile.TemporaryDirectory() as dir:
-        # Download files
-        downloader = FileDownloader(output_directory=dir,
-        # verify=verify
-        )
-        downloader.get(urls)
+        # Download and process files
+        with tempfile.TemporaryDirectory() as dir:
+            # Download files
+            downloader = FileDownloader(output_directory=dir,
+            verify=verify
+            )
+            downloader.get(urls)
 
-        # Process files
-        df = NWMFileProcessor.get_dataframe(dir)
+            # Process files
+            df = NWMFileProcessor.get_dask_dataframe(dir)
 
-    print(df.info(memory_usage="deep"))
-    print(df)
+        # Save dataframe
+        df.to_parquet(local_file)
+
+    df_sub = df[["feature_id", "streamflow"]]
+    df_max = df_sub.groupby("feature_id").max()
+    print(df_max.compute())
 
 if __name__ == "__main__":
     main()
