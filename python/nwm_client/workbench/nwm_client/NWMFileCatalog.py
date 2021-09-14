469f034ce8587a5d1ac8264a447cd7441adfde21
Jason Regina <jarq6c@gmail.com>
2021-09-14 07:26:20 -0500

    successfully swapped in google cloud platform data

--- python/nwm_client/workbench/nwm_client/NWMFileCatalog.py
+++ python/nwm_client/workbench/nwm_client/NWMFileCatalog.py
@@ -2,7 +2,13 @@
 ================
 NWM File Catalog
 ================
-Tools for discovering operational NWM NetCDF data.
+Tools for discovering operational NWM NetCDF data on generic web servers, for 
+example:
+NOMADS -- https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod/
+
+and Google Cloud Platform (GCP).
+
+GCP -- https://console.cloud.google.com/marketplace/details/noaa-public/national-water-model
 
 Classes
 -------
@@ -18,6 +24,7 @@ import asyncio
 import aiohttp
 import ssl
 from bs4 import BeautifulSoup
+from google.cloud import storage
 
 class NWMFileCatalog(ABC):
     """Abstract base class for sources of NWM file data."""
@@ -309,6 +316,80 @@ class HTTPFileCatalog(NWMFileCatalog):
         self._verify = verify
 
 class GCPFileCatalog(NWMFileCatalog):
+    """An Google Cloud client class for NWM data.
+    This GCPFileCatalog class provides various methods for discovering NWM 
+    files on Google Cloud Platform.
+    """
 
-    def list_blobs(self):
-        """Query for NWM files on Google Cloud Platform."""
+    def __init__(
+        self,
+        bucket_name: str = 'national-water-model',
+        location_metadata_mapping: pd.DataFrame = pd.DataFrame(),
+        ) -> None:
+        """Initialize catalog of NWM data source on Google Cloud Platform.
+
+        Parameters
+        ----------
+        bucket_name : str, required, default 'national-water-model'
+            Name of Google Cloud Bucket
+        location_metadata_mapping : pandas.DataFrame with nwm_feature_id Index 
+            and columns of corresponding site metadata. Defaults to 7500+ 
+            usgs_site_code used by the NWM for data assimilation.
+            
+        Returns
+        -------
+        None
+        """
+        super().__init__(location_metadata_mapping=location_metadata_mapping)
+        self.bucket_name = bucket_name
+
+    def list_blobs(
+        self,
+        configuration: str,
+        reference_time: str,
+        must_contain: str = 'channel_rt'
+        ) -> List[str]:
+        """List available blobs with provided parameters.
+
+        Parameters
+        ----------
+        configuration : str, required
+            Particular model simulation or forecast configuration. For a list 
+            of available configurations see NWMDataService.configurations
+        reference_time : str, required
+            Model simulation or forecast issuance/reference time in 
+            YYYYmmddTHHZ format.
+        must_contain : str, optional, default 'channel_rt'
+            Optional substring found in each blob name.
+
+        Returns
+        -------
+        A list of blob names that satisfy the criteria set by the parameters.
+        """
+        # Validate configuration
+        self.raise_invalid_configuration(configuration)
+
+        # Break-up reference time
+        issue_date, issue_time = NWMFileCatalog.separate_datetime(reference_time)
+
+        # Connect to bucket with anonymous client
+        client = storage.Client.create_anonymous_client()
+        bucket = client.bucket(self.bucket_name)
+
+        # Get list of blobs
+        blobs = client.list_blobs(
+            bucket,
+            prefix=f'nwm.{issue_date}/{configuration}/nwm.t{issue_time}'
+            )
+
+        # Return blob names
+        return [b.public_url for b in list(blobs) if must_contain in b.name]
+
+    @property
+    def bucket_name(self) -> str:
+        return self._bucket_name
+
+    @bucket_name.setter
+    def bucket_name(self, bucket_name: str) -> None:
+        self._bucket_name = bucket_name
+    
