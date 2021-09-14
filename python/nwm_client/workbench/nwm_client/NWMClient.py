9581e261242c65eadf2dbcd6e915d842561a1d12
Jason Regina <jarq6c@gmail.com>
2021-09-14 14:55:02 -0500

    separate method responsibilites

--- python/nwm_client/workbench/nwm_client/NWMClient.py
+++ python/nwm_client/workbench/nwm_client/NWMClient.py
@@ -12,6 +12,7 @@ NWMFileClient
 
 from abc import ABC, abstractmethod
 import pandas as pd
+import dask.dataframe as dd
 from typing import List, Union
 from pathlib import Path
 import tempfile
@@ -27,29 +28,33 @@ class NWMClient(ABC):
     def get(
         self,
         configuration: str,
-        reference_time: Union[str, List[str]] = None,
+        reference_times: List[str] = None,
         startRT: str = None,
-        endRT: str = None
-        ) -> pd.DataFrame():
+        endRT: str = None,
+        compute: bool = True
+        ) -> Union[pd.DataFrame, dd.DataFrame]:
         """Abstract method to retrieve National Water Model data as a 
-        pandas.DataFrame.
+        DataFrame.
         
         Parameters
         ----------
         configuration: str, required
             NWM configuration cycle.
-        reference_time: str or List[str], optional, default None
-            Either a single reference time or a list of reference time strings 
-            in %Y%m%dT%HZ format. E.g. 20210912T01Z
+        reference_times: str or List[str], optional, default None
+            List of reference time strings in %Y%m%dT%HZ format. 
+            e.g. ['20210912T01Z']
         startRT: str, optional
             Retrieve all available cycles starting with this reference time. 
             If endRT is not set, then retrieve from startRT until now.
         endRT: str, optional
             Used with startRT. Sets the final reference time to retrieve.
+        compute: bool, optional, default True
+            Return a pandas.DataFrame instead of a dask.dataframe.DataFrame.
 
         Returns
         -------
-        pandas.DataFrame of NWM data in hydrotools canonical format.
+        dask.dataframe.DataFrame of NWM data or a pandas.DataFrame in canonical 
+        format.
         """
 
 class NWMFileClient(NWMClient):
@@ -60,6 +65,25 @@ class NWMFileClient(NWMClient):
         catalog: NWMFileCatalog = GCPFileCatalog(),
         verify: str = None
         ) -> None:
+        """Client class for retrieving data as dataframes from a remote 
+        file-based source of National Water Model data.
+
+        Parameters
+        ----------
+        file_directory: str or pathlib.Path, optional, default None
+            Directory to save downloaded NetCDF files. If None, will use 
+            temporary files.
+        dataframe_cache: ParquetCache, default ParquetCache("nwm_cache.parquet")
+            Local parquet directory used to locally cache retrieved dataframes.
+        catalog: NWMFileCatalog, optional, default GCPFileCatalog()
+            NWMFileCatalog object used to discover NWM files.
+        verify: str, optional, default None
+            Path to CA bundle for https verification.
+
+        Returns
+        -------
+        NWMClient object
+        """
         super().__init__()
 
         # Set file output directory
@@ -80,7 +104,20 @@ class NWMFileClient(NWMClient):
         # Set CA bundle
         self.verify = verify
 
-    def download_files(self, urls, directory):
+    def download_files(self, urls: List[str], directory: Path) -> None:
+        """Download a list of urls to directory.
+
+        Parameters
+        ----------
+        urls: List[str], required
+            List of NWM NetCDF url strings.
+        directory: pathlib.Path, required
+            Path to directory where downloaded files are written.
+
+        Returns
+        -------
+        None
+        """
         # Download files
         downloader = FileDownloader(
             output_directory=directory,
@@ -88,7 +125,18 @@ class NWMFileClient(NWMClient):
             )
         downloader.get(urls)
 
-    def get_dask_dataframe(self, urls):
+    def get_dask_dataframe(self, urls: List[str]) -> dd.DataFrame:
+        """Download and merge a list of urls into a dask.dataframe.DataFrame.
+
+        Parameters
+        ----------
+        urls: List[str], required
+            List of NWM NetCDF url strings to download and combine.
+
+        Returns
+        -------
+        dask.dataframe.DataFrame of NWM data.
+        """
         # Download files to non-temp directory
         if self.file_directory:
             self.download_files(urls, self.file_directory)
@@ -99,32 +147,24 @@ class NWMFileClient(NWMClient):
             self.download_files(urls, tdir)
             return NWMFileProcessor.get_dask_dataframe(tdir)
     
-    def get(
+    def get_cycle(
         self,
         configuration: str,
-        reference_time: Union[str, List[str]] = None,
-        startRT: str = None,
-        endRT: str = None
-        ) -> pd.DataFrame():
-        """Retrieve National Water Model data as a pandas.DataFrame from a 
-        file-based data source.
+        reference_time: str
+        ) -> dd.DataFrame:
+        """Retrieve National Water Model data as a dask.dataframe.DataFrame 
+        from a file-based data source for a single cycle.
         
         Parameters
         ----------
         configuration: str, required
             NWM configuration cycle.
-        reference_time: str or List[str], optional, default None
-            Either a single reference time or a list of reference time strings 
-            in %Y%m%dT%HZ format. E.g. ['20210912T01Z']
-        startRT: str, optional
-            Retrieve all available cycles starting with this reference time. 
-            If endRT is not set, then retrieve from startRT until now.
-        endRT: str, optional
-            Used with startRT. Sets the final reference time to retrieve.
+        reference_time: str, required
+            Reference time in %Y%m%dT%HZ format. e.g. '20210912T01Z'
 
         Returns
         -------
-        pandas.DataFrame of NWM data in hydrotools canonical format.
+        dask.dataframe.DataFrame of NWM data.
         """
         # Get list of file URLs
         urls = self.catalog.list_blobs(
@@ -140,6 +180,44 @@ class NWMFileClient(NWMClient):
                 urls
             )
         return self.get_dask_dataframe(urls)
+    
+    def get(
+        self,
+        configuration: str,
+        reference_times: List[str],
+        startRT: str = None,
+        endRT: str = None,
+        compute: bool = True
+        ) -> Union[pd.DataFrame, dd.DataFrame]:
+        """Retrieve National Water Model data as a dask.dataframe.DataFrame 
+        from a file-based data source.
+        
+        Parameters
+        ----------
+        configuration: str, required
+            NWM configuration cycle.
+        reference_times: str or List[str], optional, default None
+            List of reference time strings in %Y%m%dT%HZ format. 
+            e.g. ['20210912T01Z']
+        startRT: str, optional
+            Retrieve all available cycles starting with this reference time. 
+            If endRT is not set, then retrieve from startRT until now.
+        endRT: str, optional
+            Used with startRT. Sets the final reference time to retrieve.
+        compute: bool, optional, default True
+            Return a pandas.DataFrame instead of a dask.dataframe.DataFrame.
+
+        Returns
+        -------
+        dask.dataframe.DataFrame of NWM data or a pandas.DataFrame in canonical 
+        format.
+        """
+        # Retrieve each cycle
+        dfs = []
+        for ref_time in reference_times:
+            dfs.append(self.get_cycle(configuration, ref_time))
+
+        return dd.multi.concat(dfs, ignore_unknown_divisions=True)
 
     @property
     def file_directory(self) -> Path:
