0e49aafb95d54f936a13e6d1aa89ef7e7576bfa8
Jason Regina <jarq6c@gmail.com>
2021-09-14 11:45:38 -0500

    full process

--- python/nwm_client/workbench/nwm_client/NWMClient.py
+++ python/nwm_client/workbench/nwm_client/NWMClient.py
@@ -2,12 +2,24 @@
 ================
 NWM Client Tools
 ================
+Client tools for retrieving National Water Model data from various sources.
+
+Classes
+-------
+NWMClient
+NWMFileClient
 """
 
 from abc import ABC, abstractmethod
 import pandas as pd
 from typing import List, Union
 from pathlib import Path
+import tempfile
+
+from .parquet_cache import ParquetCache
+from .FileDownloader import FileDownloader
+from .NWMFileProcessor import NWMFileProcessor
+from .NWMFileCatalog import NWMFileCatalog, GCPFileCatalog
 
 class NWMClient(ABC):
     
@@ -43,7 +55,10 @@ class NWMClient(ABC):
 class NWMFileClient(NWMClient):
     def __init__(
         self,
-        file_directory: Union[str, Path] = None
+        file_directory: Union[str, Path] = None,
+        dataframe_cache: Union[ParquetCache, None] = ParquetCache("nwm_cache.parquet"),
+        catalog: NWMFileCatalog = GCPFileCatalog(),
+        verify: str = None
         ) -> None:
         super().__init__()
 
@@ -52,6 +67,37 @@ class NWMFileClient(NWMClient):
             self.file_directory = file_directory
         else:
             self._file_directory = None
+
+        # Set dataframe cache
+        if dataframe_cache:
+            self.dataframe_cache = dataframe_cache
+        else:
+            self._dataframe_cache = None
+
+        # Set file catalog
+        self.catalog = catalog
+
+        # Set CA bundle
+        self.verify = verify
+
+    def download_files(self, urls, directory):
+        # Download files
+        downloader = FileDownloader(
+            output_directory=directory,
+            verify=self.verify
+            )
+        downloader.get(urls)
+
+    def get_dask_dataframe(self, urls):
+        # Download files to non-temp directory
+        if self.file_directory:
+            self.download_files(urls, self.file_directory)
+            return NWMFileProcessor.get_dask_dataframe(self.file_directory)
+        
+        # Download filed to temp directory
+        with tempfile.TemporaryDirectory() as tdir:
+            self.download_files(urls, tdir)
+            return NWMFileProcessor.get_dask_dataframe(tdir)
     
     def get(
         self,
@@ -69,7 +115,7 @@ class NWMFileClient(NWMClient):
             NWM configuration cycle.
         reference_time: str or List[str], optional, default None
             Either a single reference time or a list of reference time strings 
-            in %Y%m%dT%HZ format. E.g. 20210912T01Z
+            in %Y%m%dT%HZ format. E.g. ['20210912T01Z']
         startRT: str, optional
             Retrieve all available cycles starting with this reference time. 
             If endRT is not set, then retrieve from startRT until now.
@@ -80,6 +126,20 @@ class NWMFileClient(NWMClient):
         -------
         pandas.DataFrame of NWM data in hydrotools canonical format.
         """
+        # Get list of file URLs
+        urls = self.catalog.list_blobs(
+            configuration=configuration,
+            reference_time=reference_time
+            )
+
+        # Download files and get a dask DataFrame
+        if self.dataframe_cache:
+            return self.dataframe_cache.get(
+                self.get_dask_dataframe,
+                f"{configuration}/RT{reference_time}",
+                urls
+            )
+        return self.get_dask_dataframe(urls)
 
     @property
     def file_directory(self) -> Path:
@@ -88,4 +148,31 @@ class NWMFileClient(NWMClient):
     @file_directory.setter
     def file_directory(self, file_directory: Union[str, Path]) -> None:
         self._file_directory = Path(file_directory).expanduser().resolve()
-        
+        self._file_directory.mkdir(exist_ok=True, parents=True)
+
+    @property
+    def dataframe_cache(self) -> ParquetCache:
+        return self._dataframe_cache
+
+    @dataframe_cache.setter
+    def dataframe_cache(self, 
+        dataframe_cache: Union[ParquetCache, None]) -> None:
+        self._dataframe_cache = dataframe_cache
+
+    @property
+    def catalog(self) -> NWMFileCatalog:
+        return self._catalog
+
+    @catalog.setter
+    def catalog(self, 
+        catalog: NWMFileCatalog) -> None:
+        self._catalog = catalog
+
+    @property
+    def verify(self) -> str:
+        return self._verify
+
+    @verify.setter
+    def verify(self, 
+        verify: str) -> None:
+        self._verify = verify
