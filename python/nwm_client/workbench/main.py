a14d604cbd7b49c4bb8132724710802b0dc48d33
Jason Regina <jarq6c@gmail.com>
2021-09-14 13:41:26 -0500

    test local server

--- python/nwm_client/workbench/main.py
+++ python/nwm_client/workbench/main.py
@@ -1,10 +1,21 @@
 from nwm_client.NWMClient import NWMFileClient
+from nwm_client.NWMFileCatalog import HTTPFileCatalog
 
 def main():
     # Setup client with default catalog (GCP)
     client = NWMFileClient()
 
-    # Use HTTP catalog
+    # # Use HTTP catalog (local)
+    # catalog = HTTPFileCatalog(
+    #     server="http://0.0.0.0:8000/"
+    #     )
+    # client = NWMFileClient(catalog=catalog)
+
+    # # Get options
+    # configuration = "short_range"
+    # reference_time = "20210819T00Z"
+
+    # # Use HTTP catalog
     # catalog = HTTPFileCatalog(
     #     server="https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod/",
     #     verify=None
