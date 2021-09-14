65790f6ef2c70195254e6dcce14622a1730f4ddf
Jason Regina <jarq6c@gmail.com>
2021-09-14 08:33:03 -0500

    example workflow

--- python/nwm_client/workbench/main.py
+++ python/nwm_client/workbench/main.py
@@ -1,48 +1,68 @@
 from nwm_client.FileDownloader import FileDownloader
 from nwm_client.NWMFileProcessor import NWMFileProcessor
 from nwm_client.NWMFileCatalog import HTTPFileCatalog
+from nwm_client.NWMFileCatalog import GCPFileCatalog
+from nwm_client.parquet_cache import ParquetCache
 
 import tempfile
 from pathlib import Path
 import dask.dataframe as dd
+import pandas as pd
+from time import sleep
+
+def get_cycle(catalog, configuration, reference_time, verify):
+    # Get list of files
+    urls = catalog.list_blobs(
+        configuration=configuration,
+        reference_time=reference_time
+    )
+
+    # Download and process files
+    with tempfile.TemporaryDirectory() as dir:
+        # Download files
+        downloader = FileDownloader(
+            output_directory=dir,
+            verify=verify
+            )
+        downloader.get(urls)
+
+        # Process files
+        return NWMFileProcessor.get_dask_dataframe(dir)
 
 def main():
-    verify = None
-    configuration = "short_range"
-    reference_time = "20210910T00Z"
+    # Use HTTP catalog
     server = "https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod/"
-
-    # Check local file cache
-    local_file = Path(f"nwm_cache.parquet/{configuration}/RT{reference_time}")
-    if local_file.exists():
-        df = dd.read_parquet(local_file, write_index=False, compression="snappy")
-    else:
-        # Get list of files
-        catalog = HTTPFileCatalog(
-            server=server,
-            verify=verify
-            )
-        urls = catalog.list_blobs(
-            configuration=configuration,
-            reference_time=reference_time
+    verify = None
+    catalog = HTTPFileCatalog(
+        server=server,
+        verify=verify
         )
 
-        # Download and process files
-        with tempfile.TemporaryDirectory() as dir:
-            # Download files
-            downloader = FileDownloader(output_directory=dir,
+    # Use GCP Catalog
+    # catalog = GCPFileCatalog()
+
+    # Get options
+    configuration = "analysis_assim"
+    reference_time = "20210914T10Z"
+
+    # Cache options
+    root = "nwm_cache.parquet"
+    key = f"{configuration}/RT{reference_time}"
+
+    with ParquetCache(root) as cache:
+        df = cache.get(
+            get_cycle,
+            key,
+            catalog=catalog,
+            configuration=configuration,
+            reference_time=reference_time,
             verify=verify
             )
-            downloader.get(urls)
 
-            # Process files
-            df = NWMFileProcessor.get_dask_dataframe(dir)
-
-        # Save dataframe
-        df.to_parquet(local_file)
-
+    # Find maximum flows
     df_sub = df[["feature_id", "streamflow"]]
     df_max = df_sub.groupby("feature_id").max()
+    df_max = df_max[df_max > 0.0].dropna()
     print(df_max.compute())
 
 if __name__ == "__main__":
