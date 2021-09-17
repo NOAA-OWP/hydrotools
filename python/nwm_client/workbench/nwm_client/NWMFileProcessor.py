ca642b6769e8566d8af4614b38a560fdfecc7b68
Jason Regina <jarq6c@gmail.com>
2021-09-17 15:50:30 -0500

    clean up interface

--- python/nwm_client/workbench/nwm_client/NWMFileProcessor.py
+++ python/nwm_client/workbench/nwm_client/NWMFileProcessor.py
@@ -12,6 +12,8 @@ from pathlib import Path
 import xarray as xr
 import dask.dataframe as dd
 import pandas as pd
+import numpy as np
+import numpy.typing as npt
 from typing import List, Union
 
 class NWMFileProcessor:
@@ -24,6 +26,7 @@ class NWMFileProcessor:
     def get_dataset(
         cls,
         input_directory: Union[str, Path],
+        feature_id_filter: npt.ArrayLike = [],
         variables: List[str] = ["reference_time", "time", "streamflow"]
         ) -> xr.Dataset:
         """Generate an xarray.Dataset from an input directory of NWM .nc files.
@@ -33,6 +36,8 @@ class NWMFileProcessor:
         input_directory: str, pathlib.Path, required
             Directory containing collection of National Water Model NetCDF 
             files.
+        feature_id_filter: array-like, optional, default []
+            Subset of feature IDs to return.
         variables: list of str, optional, default ["reference_time", "time",
              "streamflow"]
              List of variables to retrieve from source files. Options include: 
@@ -44,77 +49,71 @@ class NWMFileProcessor:
         xarray.Dataset of input_directory lazily loaded.
         """
         # Resolve input directory
-        idir = Path(input_directory).expanduser().resolve()
+        input_directory = Path(input_directory)
 
         # Generate file list
-        file_list = [f for f in idir.glob("*.nc")]
+        file_list = [f for f in input_directory.glob("*.nc")]
 
-        # Return dataset
-        return xr.open_mfdataset(file_list)[variables]
+        # Open dataset
+        ds = xr.open_mfdataset(file_list)
+
+        # Prepare feature_id filter
+        if len(feature_id_filter) != 0:
+            # Convert to array
+            feature_id_filter = np.asarray(feature_id_filter)
+
+            # Validate filter IDs
+            check = np.isin(feature_id_filter, ds.feature_id)
+
+            # Subset by feature ID and variable
+            return ds.sel(feature_id=feature_id_filter[check])[variables]
+
+        # Subset by variable only
+        return ds[variables]
 
     @classmethod
-    def get_dask_dataframe(
+    def convert_to_dask_dataframe(
         cls,
-        input_directory: Union[str, Path],
-        variables: List[str] = ["reference_time", "time", "streamflow"]
+        ds: xr.Dataset
         ) -> dd.DataFrame:
-        """Generate a dask.dataframe.DataFrame from an input directory of NWM 
-        .nc files.
+        """Generate a dask.dataframe.DataFrame from an xarray.Dataset.
 
         Parameters
         ----------
-        input_directory: str, pathlib.Path, required
-            Directory containing collection of National Water Model NetCDF 
-            files.
-        variables: list of str, optional, default ["reference_time", "time",
-             "streamflow"]
-             List of variables to retrieve from source files. Options include: 
-             'time', 'reference_time', 'feature_id', 'crs', 'streamflow', 
-             'nudge', 'velocity', 'qSfcLatRunoff', 'qBucket', 'qBtmVertRunoff'
+        ds: xarray.Dataset, required
+            xarray.Dataset containing National Water Model data.
 
         Returns
         -------
-        dask.dataframe.DataFrame of input_directory
+        dask.dataframe.DataFrame of NWM data.
         """
-        # Retrieve dataset
-        with cls.get_dataset(input_directory, variables) as ds:
-            # Convert to dask dataframe
-            df = ds.to_dask_dataframe()
+        # Count feature IDs
+        no_feature_ids = len(ds.feature_id)
 
-            # Add and categorize configuration column
-            df["configuration"] = ds.attrs["model_configuration"]
-            df["configuration"] = df["configuration"].astype("category")
+        # Convert to dask dataframe
+        df = ds.to_dask_dataframe()
 
-            # Downcast floats
-            float_columns = df.select_dtypes(include=["float"])
-            for col in float_columns:
-                df[col] = pd.to_numeric(df[col], downcast="float")
-
-            # Return dataframe
-            return df
+        # Repartition by feature ID
+        df = df.sort_values(by="feature_id")
+        df = df.set_index("feature_id")
+        df = df.repartition(npartitions=no_feature_ids)
+        return df.reset_index()
 
     @classmethod
-    def get_dataframe(
+    def convert_to_dataframe(
         cls,
-        input_directory: Union[str, Path],
-        variables: List[str] = ["reference_time", "time", "streamflow"]
+        ds: xr.Dataset
         ) -> pd.DataFrame:
-        """Generate a pandas.DataFrame from an input directory of NWM .nc files.
+        """Generate a pandas.DataFrame from an xarray.Dataset.
 
         Parameters
         ----------
-        input_directory: str, pathlib.Path, required
-            Directory containing collection of National Water Model NetCDF 
-            files.
-        variables: list of str, optional, default ["reference_time", "time",
-             "streamflow"]
-             List of variables to retrieve from source files. Options include: 
-             'time', 'reference_time', 'feature_id', 'crs', 'streamflow', 
-             'nudge', 'velocity', 'qSfcLatRunoff', 'qBucket', 'qBtmVertRunoff'
+        ds: xarray.Dataset, required
+            xarray.Dataset containing National Water Model data.
 
         Returns
         -------
-        pandas.DataFrame of input_directory data
+        pandas.DataFrame of NWM data.
         """
-        # Retrieve dask dataframe, compute pandas dataframe, and return
-        return cls.get_dask_dataframe(input_directory, variables).compute()
+        # Compute pandas dataframe
+        return cls.convert_to_dask_dataframe(ds).compute()
