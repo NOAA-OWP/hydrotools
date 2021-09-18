5e82cabe054788bdaa7dbb6ca07ad2f27b3bbcbb
Jason Regina <jarq6c@gmail.com>
2021-09-18 12:30:38 -0500

    use http file catalog

--- python/nwm_client/workbench/main.py
+++ python/nwm_client/workbench/main.py
@@ -1,12 +1,21 @@
 from nwm_client.NWMClient import NWMFileClient
+from nwm_client.NWMFileCatalog import HTTPFileCatalog
 
 if __name__ == "__main__":
-    client = NWMFileClient()
+    # Setup catalog
+    catalog = HTTPFileCatalog(
+        server="https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod/"
+    )
 
+    # Setup client
+    client = NWMFileClient(catalog=catalog)
+
+    # Retrieve data
     df = client.get(
-        configuration="medium_range_mem1",
-        reference_times=["20210915T00Z"],
+        configuration="analysis_assim",
+        reference_times=["20210918T00Z"],
         compute=False
         )
 
+    # Look at data
     print(df)
