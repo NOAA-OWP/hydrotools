3a5bfdb1ae28c4fcbf6eb1c882e635ed565a2fac
Jason Regina <jarq6c@gmail.com>
2021-09-30 15:01:23 -0500

    basic tests

---
+++ python/nwm_client_new/tests/test_FileDownloader.py
@@ -0,0 +1,21 @@
+import pytest
+from hydrotools.nwm_client.FileDownloader import FileDownloader
+from tempfile import TemporaryDirectory
+
+def test_parameters():
+    with TemporaryDirectory() as td:
+        downloader = FileDownloader(output_directory=td)
+        assert downloader.output_directory
+        assert not downloader.create_directory
+        assert downloader.ssl_context
+        assert downloader.limit == 10
+
+@pytest.mark.slow
+def test_get():
+    with TemporaryDirectory() as td:
+        downloader = FileDownloader(output_directory=td)
+        downloader.get(
+            ["https://pandas.pydata.org/docs/user_guide/index.html"]
+            )
+        ofile = downloader.output_directory / "index.html"
+        assert ofile.exists()
---
+++ python/nwm_client_new/tests/test_NWMClient.py
@@ -0,0 +1,54 @@
+import pytest
+from hydrotools.nwm_client.NWMClient import NWMFileClient
+from hydrotools.nwm_client.NWMFileCatalog import HTTPFileCatalog
+import pandas as pd
+
+# Set reference time
+yesterday = pd.Timestamp.utcnow() - pd.Timedelta("1D")
+reference_time_gcp = yesterday.strftime("%Y%m%dT%-HZ")
+reference_time_http = (yesterday+pd.Timedelta("1H")).strftime("%Y%m%dT%-HZ")
+
+# Canonical columns
+canonical_columns = [
+    "nwm_feature_id",
+    "reference_time",
+    "usgs_site_code",
+    "value_time",
+    "measurement_unit",
+    "variable_name",
+    "value",
+    "configuration"
+]
+
+@pytest.fixture
+def setup_gcp():
+    return NWMFileClient()
+
+@pytest.fixture
+def setup_http():
+    catalog = HTTPFileCatalog(
+        server="https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod/"
+        )
+    return NWMFileClient(catalog=catalog)
+
+@pytest.mark.slow
+def test_gcp_client(setup_gcp):
+    df = setup_gcp.get(
+        configuration="analysis_assim",
+        reference_times=[reference_time_gcp]
+    ).head()
+    for col in canonical_columns:
+        assert col in df
+    assert "analysis_assim" in df["configuration"].values
+    assert "streamflow" in df["variable_name"].values
+
+@pytest.mark.slow
+def test_http_client(setup_http):
+    df = setup_http.get(
+        configuration="analysis_assim",
+        reference_times=[reference_time_http]
+    ).head()
+    for col in canonical_columns:
+        assert col in df
+    assert "analysis_assim" in df["configuration"].values
+    assert "streamflow" in df["variable_name"].values
