0871dd7fcb2d857436d17bda4fe57db5eaf7a01c
Austin Raney <aaraney@crimson.ua.edu>
2021-10-26 15:12:05 -0400

    import __version__ in __init__.py. needed by setuptools for ast parsing. see setuptools #1724 for more information

--- python/_restclient/src/hydrotools/_restclient/__init__.py
+++ python/_restclient/src/hydrotools/_restclient/__init__.py
@@ -1,3 +1,6 @@
+# removing __version__ import will cause build to fail. see: https://github.com/pypa/setuptools/issues/1724#issuecomment-627241822
+from ._version import __version__
+
 from ._restclient import RestClient
 from .utilities import Alias, AliasGroup
 from .urllib import Url, Variadic
