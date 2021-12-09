e80f6293b99d1c53943ea05c531fd28aaa38a7cb
Jason Regina <jarq6c@gmail.com>
2021-12-09 09:29:05 -0600

    test for invalid values

--- python/metrics/tests/test_metrics.py
+++ python/metrics/tests/test_metrics.py
@@ -19,6 +19,20 @@ alt_contigency_table = {
     'TN': 4
 }
 
+zero_contingency_table = {
+    'true_positive': 0,
+    'false_positive': 0,
+    'false_negative': 0,
+    'true_negative': 0
+}
+
+nan_contigency_table = {
+    'true_positive': np.nan,
+    'false_positive': np.nan,
+    'false_negative': np.nan,
+    'true_negative': np.nan
+}
+
 y_true = [1., 2., 3., 4.]
 y_pred = [4., 3., 2., 1.]
 
@@ -57,6 +71,13 @@ def test_probability_of_detection():
         )
     assert POD == (1/4)
 
+    with pytest.warns(RuntimeWarning):
+        POD = metrics.probability_of_detection(zero_contingency_table)
+        assert np.isnan(POD)
+
+    POD = metrics.probability_of_detection(nan_contigency_table)
+    assert np.isnan(POD)
+
 def test_probability_of_false_detection():
     POFD = metrics.probability_of_false_detection(contigency_table)
     assert POFD == (2/6)
@@ -66,6 +87,13 @@ def test_probability_of_false_detection():
         true_negative_key='TN'
         )
     assert POFD == (2/6)
+
+    with pytest.warns(RuntimeWarning):
+        POFD = metrics.probability_of_false_detection(zero_contingency_table)
+        assert np.isnan(POFD)
+
+    POFD = metrics.probability_of_false_detection(nan_contigency_table)
+    assert np.isnan(POFD)
 
 def test_probability_of_false_alarm():
     POFA = metrics.probability_of_false_alarm(contigency_table)
@@ -77,6 +105,13 @@ def test_probability_of_false_alarm():
         false_positive_key='FP'
         )
     assert POFA == (2/3)
+
+    with pytest.warns(RuntimeWarning):
+        POFA = metrics.probability_of_false_alarm(zero_contingency_table)
+        assert np.isnan(POFA)
+
+    POFA = metrics.probability_of_false_alarm(nan_contigency_table)
+    assert np.isnan(POFA)
 
 def test_threat_score():
     TS = metrics.threat_score(contigency_table)
@@ -90,6 +125,13 @@ def test_threat_score():
         )
     assert TS == (1/6)
 
+    with pytest.warns(RuntimeWarning):
+        TS = metrics.threat_score(zero_contingency_table)
+        assert np.isnan(TS)
+
+    TS = metrics.threat_score(nan_contigency_table)
+    assert np.isnan(TS)
+
 def test_frequency_bias():
     FBI = metrics.frequency_bias(contigency_table)
     assert FBI == (3/4)
@@ -100,6 +142,13 @@ def test_frequency_bias():
         false_negative_key='FN'
         )
     assert FBI == (3/4)
+
+    with pytest.warns(RuntimeWarning):
+        FBI = metrics.frequency_bias(zero_contingency_table)
+        assert np.isnan(FBI)
+
+    FBI = metrics.frequency_bias(nan_contigency_table)
+    assert np.isnan(FBI)
 
 def test_percent_correct():
     PC = metrics.percent_correct(contigency_table)
@@ -113,6 +162,13 @@ def test_percent_correct():
         )
     assert PC == (5/10)
 
+    with pytest.warns(RuntimeWarning):
+        PC = metrics.percent_correct(zero_contingency_table)
+        assert np.isnan(PC)
+
+    PC = metrics.percent_correct(nan_contigency_table)
+    assert np.isnan(PC)
+
 def test_base_chance():
     a_r = metrics.base_chance(contigency_table)
     assert a_r == (12/10)
@@ -125,6 +181,13 @@ def test_base_chance():
         )
     assert a_r == (12/10)
 
+    with pytest.warns(RuntimeWarning):
+        a_r = metrics.base_chance(zero_contingency_table)
+        assert np.isnan(a_r)
+
+    a_r = metrics.base_chance(nan_contigency_table)
+    assert np.isnan(a_r)
+
 def test_equitable_threat_score():
     ETS = metrics.equitable_threat_score(contigency_table)
     assert isclose(ETS, (-0.2/4.8), abs_tol=0.000001)
@@ -136,6 +199,13 @@ def test_equitable_threat_score():
         true_negative_key='TN'
         )
     assert isclose(ETS, (-0.2/4.8), abs_tol=0.000001)
+
+    with pytest.warns(RuntimeWarning):
+        ETS = metrics.equitable_threat_score(zero_contingency_table)
+        assert np.isnan(ETS)
+
+    ETS = metrics.equitable_threat_score(nan_contigency_table)
+    assert np.isnan(ETS)
 
 def test_mean_squared_error():
     MSE = metrics.mean_squared_error(y_true, y_pred)
@@ -159,4 +229,3 @@ def test_nash_sutcliffe_efficiency():
     NNSEL = metrics.nash_sutcliffe_efficiency(np.exp(y_true), 
         np.exp(y_pred), log=True, normalized=True)
     assert NNSEL == 0.2
-    print(NNSEL)
