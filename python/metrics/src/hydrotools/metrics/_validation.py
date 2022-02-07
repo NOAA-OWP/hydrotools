9e69e989c2020cd95898f1e0070cce4bd7d4c9c0
Jason Regina <jarq6c@gmail.com>
2022-02-07 15:10:40 -0600

    add custom errors

--- python/metrics/src/hydrotools/metrics/_validation.py
+++ python/metrics/src/hydrotools/metrics/_validation.py
@@ -13,7 +13,35 @@ Functions
 
 import numpy as np
 import numpy.typing as npt
-from typing import List
+from typing import List, Tuple
+
+class InconsistentShapesError(Exception):
+    def __init__(self,
+        *args: object,
+        array_shape_1: Tuple[int],
+        array_shape_2: Tuple[int]
+        ) -> None:
+        self._array_shape_1 = array_shape_1
+        self._array_shape_2 = array_shape_2
+        super().__init__(*args)
+
+    def __str__(self) -> str:
+        a1s = self._array_shape_1
+        a2s = self._array_shape_2
+        message = f"Arrays are not the same shape. The array shapes are {a1s} and {a2s}."
+        return message
+
+class NonVectorError(Exception):
+    def __init__(self,
+        *args: object,
+        arr: npt.ArrayLike
+        ) -> None:
+        self._arr = arr
+        super().__init__(*args)
+
+    def __str__(self) -> str:
+        message = f"This array is not 1-dimensional\n {self._arr}"
+        return message
 
 def raise_for_non_vector(
     *arrays: List[npt.ArrayLike]
@@ -28,14 +56,13 @@ def raise_for_non_vector(
 
     Raises
     ------
-    ValueError:
+    NonVectorError:
         Raises if one of the arrays is not 1-dimensional.
     """
     # Check each array
     for x in arrays:
         if len(np.array(x).shape) != 1:
-            message = "all input arrays must be 1-dimensional"
-            raise ValueError(message)
+            raise NonVectorError(arr=x)
 
 def raise_for_inconsistent_shapes(
     *arrays: List[npt.ArrayLike]
@@ -50,7 +77,7 @@ def raise_for_inconsistent_shapes(
 
     Raises
     ------
-    ValueError:
+    InconsistentShapesError:
         Raises if all of the arrays are not the same shape.
     """
     # Convert to numpy arrays
@@ -64,5 +91,7 @@ def raise_for_inconsistent_shapes(
         # Test each array
         test = all(i == j for i, j in zip(x.shape, y.shape))
         if not test:
-            message = "all input arrays must be the same shape"
-            raise ValueError(message)
+            raise InconsistentShapesError(
+                array_shape_1=x.shape,
+                array_shape_2=y.shape
+                )
