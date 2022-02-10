834dfbf726d263854514390d7fa4bea41f8332dd
Austin Raney <aaraney@protonmail.com>
2022-02-10 18:53:47 -0500

    catch and raise meaningful exception when gcp client has not been installed

--- python/nwm_client/src/hydrotools/nwm_client/gcp.py
+++ python/nwm_client/src/hydrotools/nwm_client/gcp.py
@@ -17,7 +17,14 @@ Classes
 from pandas.core.indexing import convert_from_missing_indexer_tuple
 from hydrotools.caches.hdf import HDFCache
 
-from google.cloud import storage
+try:
+    from google.cloud import storage
+except ImportError as e:
+    error_message = ("Unable to import google-cloud-storage. Reinstall `hydrotools.nwm_client` using the extra-requirement, `gcp`."
+                     "\n"
+                     "`pip install 'hydrotools.nwm_client[gcp]'")
+    raise ImportError(error_message) from e
+
 from io import BytesIO
 import xarray as xr
 import warnings
