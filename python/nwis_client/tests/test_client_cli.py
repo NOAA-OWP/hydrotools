0512e4b10052d51dacc623b8d1708e26c4ba76ec
Jason Regina <jarq6c@gmail.com>
2022-03-04 17:41:21 -0600

    CLiRunner does not like async

--- python/nwis_client/tests/test_client_cli.py
+++ python/nwis_client/tests/test_client_cli.py
@@ -1,22 +1,25 @@
 import pytest
-from click.testing import CliRunner
-from hydrotools.nwis_client.cli import run
+from hydrotools.nwis_client import cli
 from pathlib import Path
+import subprocess
+from tempfile import TemporaryDirectory
 
 def test_cli():
-    runner = CliRunner()
-
-    with runner.isolated_filesystem():
+    """Normaly would use click.testing.CLiRunner. However, this does not appear to be async friendly."""
+    with TemporaryDirectory() as tdir:
         # Test default parameters
-        result1 = runner.invoke(run, ['01013500', '02146470', "test_output_1.csv"])
-        assert result1.exit_code == 0
-        assert Path("test_output_1.csv").exists()
+        result1 = subprocess.run([
+            "nwis-client", "01013500", "02146470", f"{tdir}/test_output_1.csv"
+        ])
+        assert result1.returncode == 0
+        assert Path(f"{tdir}/test_output_1.csv").exists()
 
         # Test other parameters
-        result2 = runner.invoke(run, [
+        result2 = subprocess.run([
+            "nwis-client",
             '-s', '2022-01-01',
             '-e', '2022-01-02',
             '-p', '00065',
-            '01013500', '02146470', "test_output_2.csv"])
-        assert result2.exit_code == 0
-        assert Path("test_output_2.csv").exists()
+            '01013500', '02146470', f"{tdir}/test_output_2.csv"])
+        assert result2.returncode == 0
+        assert Path(f"{tdir}/test_output_2.csv").exists()
