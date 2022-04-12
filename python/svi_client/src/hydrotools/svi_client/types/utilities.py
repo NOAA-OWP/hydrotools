9e2bb3633ef4075df2468e59d4d53969ce66711d
Austin Raney <aaraney@protonmail.com>
2022-04-12 10:58:13 -0400

    raise value errors when validating utility types

--- python/svi_client/src/hydrotools/svi_client/types/utilities.py
+++ python/svi_client/src/hydrotools/svi_client/types/utilities.py
@@ -9,21 +9,31 @@ def validate_location(location: str) -> str:
     location_key = LOCATIONS.get(location.lower())  # noqa
 
     if location_key is None:
-        ...
+        valid_locations = sorted(list(LOCATIONS.keys.union(LOCATIONS.values)))
+        error_message = f"Invalid location: {location}. Valid location values are\n{valid_locations}"
+        raise ValueError(error_message)
 
     return location_key
 
 
 def validate_geographic_scale(geographic_scale: GeographicScale) -> str:
-    if geographic_scale not in typing.get_args(GeographicScale):
-        ...
+    valid_geo_scales = typing.get_args(GeographicScale)
+
+    if geographic_scale not in valid_geo_scales:
+        valid_geo_scales = sorted(valid_geo_scales)
+        error_message = f"Invalid geographic scale: {geographic_scale}. Valid geographic scale values are\n{valid_geo_scales}"
+        raise ValueError(error_message)
 
     return geographic_scale
 
 
 def validate_geographic_context(geographic_context: GeographicContext) -> str:
-    if geographic_context not in typing.get_args(GeographicContext):
-        ...
+    valid_geo_contexts = typing.get_args(GeographicContext)
+
+    if geographic_context not in valid_geo_contexts:
+        valid_geo_scales = sorted(valid_geo_contexts)
+        error_message = f"Invalid geographic context: {geographic_context}. Valid geographic context values are\n{valid_geo_scales}"
+        raise ValueError(error_message)
 
     return geographic_context
 
@@ -31,7 +41,10 @@ def validate_geographic_context(geographic_context: GeographicContext) -> str:
 def validate_year(year: Year) -> str:
     year_str = str(year)
 
-    if year_str not in typing.get_args(Year):
-        ...
+    valid_years = typing.get_args(Year)
+    if year_str not in valid_years:
+        valid_years = sorted(valid_years)
+        error_message = f"Invalid year: {year}. Valid year values are\n{valid_years}"
+        raise ValueError(error_message)
 
     return year_str
