0d28fba528217351880e4b6e2966caa37fcf3982
Jason Regina <jarq6c@gmail.com>
2022-08-05 12:20:41 -0500

    update file processor tests

--- python/nwm_client_new/tests/test_NWMFileProcessor.py
+++ python/nwm_client_new/tests/test_NWMFileProcessor.py
@@ -5,22 +5,24 @@ from pathlib import Path
 input_directory = Path(__file__).parent / "short_range"
 
 def test_get_dataset():
-    ds = NWMFileProcessor.get_dataset(input_directory)
-    assert "reference_time" in ds
-    assert "time" in ds
-    assert "streamflow" in ds
-    ds.close()
+    with pytest.warns(UserWarning):
+        ds = NWMFileProcessor.get_dataset(input_directory)
+        assert "reference_time" in ds
+        assert "time" in ds
+        assert "streamflow" in ds
+        ds.close()
 
 @pytest.mark.slow
 def test_convert_to_dask_dataframe():
-    ds = NWMFileProcessor.get_dataset(input_directory)
+    ds = NWMFileProcessor.get_dataset(input_directory, feature_id_filter=[])
     df = NWMFileProcessor.convert_to_dask_dataframe(ds)
     assert df.npartitions == 2
     ds.close()
 
 @pytest.mark.slow
 def test_convert_to_dataframe():
-    ds = NWMFileProcessor.get_dataset(input_directory)
-    df = NWMFileProcessor.convert_to_dataframe(ds)
-    assert "feature_id" in df
-    ds.close()
+    with pytest.warns(UserWarning):
+        ds = NWMFileProcessor.get_dataset(input_directory)
+        df = NWMFileProcessor.convert_to_dataframe(ds)
+        assert "feature_id" in df
+        ds.close()
