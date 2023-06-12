1fca12b0c83e412157d20742dd24a44815f9917b
Jason Regina <jarq6c@gmail.com>
2023-06-12 14:19:35 -0500

    update file downloader tests

--- python/nwm_client_new/tests/test_FileDownloader.py
+++ python/nwm_client_new/tests/test_FileDownloader.py
@@ -19,3 +19,16 @@ def test_get():
             )
         ofile = downloader.output_directory / "index.html"
         assert ofile.exists()
+
+@pytest.mark.slow
+def test_overwrite_warning():
+    with TemporaryDirectory() as td:
+        with pytest.warns(UserWarning):
+            downloader = FileDownloader(output_directory=td)
+            downloader.get(
+                [("https://pandas.pydata.org/docs/user_guide/index.html","index.html")]
+                )
+            ofile = downloader.output_directory / "index.html"
+            downloader.get(
+                [("https://pandas.pydata.org/docs/user_guide/index.html","index.html")]
+                )
