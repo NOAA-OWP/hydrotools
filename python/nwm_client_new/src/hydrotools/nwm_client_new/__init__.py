57729a780c31130346f73c549d4a7a29621c4735
jarq6c <jason.regina@noaa.gov>
2024-12-03 08:59:04 -0600

    add warning about package name change

--- python/nwm_client_new/src/hydrotools/nwm_client_new/__init__.py
+++ python/nwm_client_new/src/hydrotools/nwm_client_new/__init__.py
@@ -1,2 +1,9 @@
 # removing __version__ import will cause build to fail. see: https://github.com/pypa/setuptools/issues/1724#issuecomment-627241822
 from ._version import __version__
+
+import warnings
+
+warnings.warn(
+    "nwm_client_new will be renamed to nwm_client for v8.0+. Update imports as necessary.",
+    FutureWarning
+    )
