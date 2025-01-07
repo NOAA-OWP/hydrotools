a3d8ebc161aa3740cb36f997dcd4a2db23cb433f
jarq6c <jason.regina@noaa.gov>
2025-01-07 09:23:00 -0600

    bump release to 3.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021-2024, Jason A. Regina and Austin Raney'
 author = 'Jason A. Regina and Austin Raney'
 
 # The full version, including alpha/beta/rc tags
-release = '2.4.0'
+release = '3.0.0'
 
 
 # -- General configuration ---------------------------------------------------
