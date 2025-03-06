e6260ce6a636e5b2a629e1286db1edeceabc7d28
jason.regina <jason.regina@noaa.gov>
2025-03-06 18:53:22 +0000

    split up tests

--- python/events/tests/test_interception.py
+++ python/events/tests/test_interception.py
@@ -14,46 +14,73 @@ def test_compute_interception() -> None:
     # Errors
     with pytest.raises(AssertionError):
         itc.compute_interception(0.0, 5.0, 0.5)
-        
+
     with pytest.raises(AssertionError):
         itc.compute_interception(50.0, 0.0, 0.5)
 
     with pytest.raises(AssertionError):
         itc.compute_interception(50.0, 5.0, 0.0)
 
-def test_compute_canopy_interception() -> None:
+def test_compute_canopy_saturation() -> None:
+    """Tests basic functionality of a method to compute the maximum amount of
+    canopy interception. Tests whether method correctly raises with bad inputs.
+    The method should raise for 0.0 rainfall rate, canopy fraction, or
+    evaporation rate."""
     estimate = itc.compute_canopy_saturation(5.0, 0.2, 0.2, 0.3)
     assert np.isclose(estimate, 0.2, atol=0.1)
 
     # Errors
-    # _ = itc.compute_canopy_saturation(0.0, 0.2, 0.2, 0.3)
-    # _ = itc.compute_canopy_saturation(5.0, 0.0, 0.2, 0.3)
-    # _ = itc.compute_canopy_saturation(5.0, 0.4, 0.2, 0.0)
+    with pytest.raises(AssertionError):
+        itc.compute_canopy_saturation(0.0, 0.2, 0.2, 0.3)
 
+    with pytest.raises(AssertionError):
+        itc.compute_canopy_saturation(5.0, 0.0, 0.2, 0.3)
+
+    with pytest.raises(AssertionError):
+        itc.compute_canopy_saturation(5.0, 0.4, 0.2, 0.0)
+
+def test_compute_trunk_saturation() -> None:
     estimate = itc.compute_trunk_saturation(5.0, 0.5, 0.2, 0.3, 0.02, 0.02, 0.02)
     assert np.isclose(estimate, 2.3, atol=0.1)
 
     # Errors
-    # _ = itc.compute_trunk_saturation(0.0, 0.5, 0.2, 0.3, 0.02, 0.02, 0.02)
-    # _ = itc.compute_trunk_saturation(5.0, 0.0, 0.2, 0.3, 0.02, 0.02, 0.02)
-    # _ = itc.compute_trunk_saturation(5.0, 0.5, 0.2, 0.3, 0.02, 0.0, 0.02)
+    with pytest.raises(AssertionError):
+        itc.compute_trunk_saturation(0.0, 0.5, 0.2, 0.3, 0.02, 0.02, 0.02)
 
+    with pytest.raises(AssertionError):
+        itc.compute_trunk_saturation(5.0, 0.0, 0.2, 0.3, 0.02, 0.02, 0.02)
+
+    with pytest.raises(AssertionError):
+        itc.compute_trunk_saturation(5.0, 0.5, 0.2, 0.3, 0.02, 0.0, 0.02)
+
+def test_compute_canopy_loss() -> None:
     estimate = itc.compute_canopy_loss(50.0, 5.0, 0.2, 0.5, 0.3, 0.02)
     assert np.isclose(estimate, 1.5, atol=0.1)
 
     # Errors
-    # _ = itc.compute_canopy_loss(0.0, 5.0, 0.2, 0.5, 0.3, 0.02)
-    # _ = itc.compute_canopy_loss(50.0, 0.0, 0.2, 0.5, 0.3, 0.02)
-    # _ = itc.compute_canopy_loss(50.0, 5.0, 0.2, 0.0, 0.3, 0.02)
+    with pytest.raises(AssertionError):
+        itc.compute_canopy_loss(0.0, 5.0, 0.2, 0.5, 0.3, 0.02)
 
+    with pytest.raises(AssertionError):
+        itc.compute_canopy_loss(50.0, 0.0, 0.2, 0.5, 0.3, 0.02)
+
+    with pytest.raises(AssertionError):
+        itc.compute_canopy_loss(50.0, 5.0, 0.2, 0.0, 0.3, 0.02)
+
+def test_compute_trunk_loss() -> None:
     estimate = itc.compute_trunk_loss(
         5.0, 5.0, 0.2, 0.5, 0.3, 0.02, 0.02, 0.02, 0.2)
     assert np.isclose(estimate, 0.04, atol=0.01)
 
     # Errors
-    # _ = itc.compute_trunk_loss(
-    #     0.0, 5.0, 0.2, 0.5, 0.3, 0.02, 0.02, 0.02, 0.2)
-    # _ = itc.compute_trunk_loss(
-    #     5.0, 0.0, 0.2, 0.5, 0.3, 0.02, 0.02, 0.02, 0.2)
-    # _ = itc.compute_trunk_loss(
-    #     5.0, 5.0, 0.2, 0.0, 0.3, 0.02, 0.02, 0.02, 0.2)
+    with pytest.raises(AssertionError):
+        itc.compute_trunk_loss(
+            0.0, 5.0, 0.2, 0.5, 0.3, 0.02, 0.02, 0.02, 0.2)
+        
+    with pytest.raises(AssertionError):
+        itc.compute_trunk_loss(
+            5.0, 0.0, 0.2, 0.5, 0.3, 0.02, 0.02, 0.02, 0.2)
+        
+    with pytest.raises(AssertionError):
+        itc.compute_trunk_loss(
+            5.0, 5.0, 0.2, 0.0, 0.3, 0.02, 0.02, 0.02, 0.2)
