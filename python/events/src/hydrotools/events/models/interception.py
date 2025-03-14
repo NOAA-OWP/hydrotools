6206abd18f2b5bec83b3959c26bc261d5c916edb
jarq6c <jason.regina@noaa.gov>
2025-03-14 13:55:35 -0500

    add more default parameters

--- python/events/src/hydrotools/events/models/interception.py
+++ python/events/src/hydrotools/events/models/interception.py
@@ -30,16 +30,60 @@ compute_trunk_saturation
 compute_canopy_loss
 compute_trunk_loss
 compute_interception
+
+Classes
+-------
+DefaultParameters
 """
 
+from dataclasses import dataclass
 import warnings
 import numpy as np
+
+@dataclass
+class DefaultParameters:
+    """
+    Dataclass used to store module level default parameters.
+
+    Parameters
+    ----------
+    canopy_storage: float, optional, default 1.2
+        Minimum depth of water required to saturate the canopy. Typically 1.2
+        mm (Miralles et al, 2010). Notated $S_c$. Expected to vary little by
+        location and leaf type (± 0.4 mm, standard deviation). Measurement
+        units should correspond to rainfall_rate (i.e. if rainfall_rate is in
+        mm/hour, then canopy_storage should be in mm).
+    evaporation_rate: float, optional, default 0.3
+        Average evaporation rate. Typically 0.3 mm/hour (Miralles et al, 2010).
+        Notated $E_c$. Expected to vary little between locations
+        (± 0.1 mm/hour, standard deviation). Should be in the same measurement
+        units as rainfall_rate.
+    trunk_fraction: float, optional, default 0.02
+        Fraction of rainfall hitting the "trunks" under the canopy. Typically
+        0.02 (Miralles et al, 2010). Notated $p_d$. Unitless.
+    trunk_evaporation: float, optional, default 0.02
+        Fraction of evaporation originating from vegetation "trunks." Typically
+        0.02 (Miralles et al, 2010). Notated $\\epsilon$. Unitless.
+    trunk_capacity: float, optional, default 0.02
+        Minimum depth of water required to saturate trunks under the canopy.
+        Typically 0.02 mm (Miralles et al, 2010). Notated $S_t$. Measurement
+        units should correspond to rainfall_rate (i.e. if rainfall_rate is in
+        mm/hour, then trunk_capacity should be in mm).
+    """
+    canopy_storage: float = 1.2
+    evaporation_rate: float = 0.3
+    trunk_fraction: float = 0.02
+    trunk_evaporation: float = 0.02
+    trunk_capacity: float = 0.02
+
+_DEFAULT_PARAMETERS: DefaultParameters = DefaultParameters()
+"""Module level default parameters."""
 
 def compute_canopy_saturation(
         rainfall_rate: float,
         canopy_storage: float,
-        trunk_evaporation: float,
-        evaporation_rate: float
+        trunk_evaporation: float = _DEFAULT_PARAMETERS.trunk_evaporation,
+        evaporation_rate: float = _DEFAULT_PARAMETERS.evaporation_rate
 ) -> float:
     """
     Compute the amount of rainfall necessary to saturate the canopy.
@@ -94,10 +138,10 @@ def compute_trunk_saturation(
         rainfall_rate: float,
         canopy_fraction: float,
         canopy_saturation: float,
-        evaporation_rate: float,
-        trunk_capacity: float,
-        trunk_fraction: float,
-        trunk_evaporation: float
+        evaporation_rate: float = _DEFAULT_PARAMETERS.evaporation_rate,
+        trunk_capacity: float = _DEFAULT_PARAMETERS.trunk_capacity,
+        trunk_fraction: float = _DEFAULT_PARAMETERS.trunk_fraction,
+        trunk_evaporation: float = _DEFAULT_PARAMETERS.trunk_evaporation
 ) -> float:
     """
     Compute the amount of rainfall necessary to saturate the trunks under
@@ -164,8 +208,8 @@ def compute_canopy_loss(
         rainfall_rate: float,
         canopy_saturation: float,
         canopy_fraction: float,
-        evaporation_rate: float,
-        trunk_evaporation: float
+        evaporation_rate: float = _DEFAULT_PARAMETERS.evaporation_rate,
+        trunk_evaporation: float = _DEFAULT_PARAMETERS.trunk_evaporation
 ) -> float:
     """
     Compute the amount of canopy intercepted rainfall. Typically in mm.
@@ -223,11 +267,11 @@ def compute_trunk_loss(
         rainfall_rate: float,
         canopy_saturation: float,
         canopy_fraction: float,
-        evaporation_rate: float,
         trunk_saturation: float,
-        trunk_capacity: float,
-        trunk_fraction: float,
-        trunk_evaporation: float
+        evaporation_rate: float = _DEFAULT_PARAMETERS.evaporation_rate,
+        trunk_capacity: float = _DEFAULT_PARAMETERS.trunk_capacity,
+        trunk_fraction: float = _DEFAULT_PARAMETERS.trunk_fraction,
+        trunk_evaporation: float = _DEFAULT_PARAMETERS.trunk_evaporation
 ) -> float:
     """
     Compute the amount of trunk intercepted rainfall. Typically in mm.
@@ -295,11 +339,11 @@ def compute_interception(
         gross_rainfall: float,
         rainfall_rate: float,
         canopy_fraction: float,
-        canopy_storage: float = 1.2,
-        evaporation_rate: float = 0.3,
-        trunk_fraction: float = 0.02,
-        trunk_evaporation: float = 0.02,
-        trunk_capacity: float = 0.02
+        canopy_storage: float = _DEFAULT_PARAMETERS.canopy_storage,
+        evaporation_rate: float = _DEFAULT_PARAMETERS.evaporation_rate,
+        trunk_fraction: float = _DEFAULT_PARAMETERS.trunk_fraction,
+        trunk_evaporation: float = _DEFAULT_PARAMETERS.trunk_evaporation,
+        trunk_capacity: float = _DEFAULT_PARAMETERS.trunk_capacity
 ) -> float:
     """
     Compute the amount of total intercepted rainfall. Typically in mm.
