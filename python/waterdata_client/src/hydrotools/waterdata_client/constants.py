91e87b71f85642e871183709e9e200ead69979b6
jason.regina <jason.regina@noaa.gov>
2026-04-15 15:46:00 +0000

    add generated module and tests

--- python/waterdata_client/src/hydrotools/waterdata_client/constants.py
+++ python/waterdata_client/src/hydrotools/waterdata_client/constants.py
@@ -1,4 +1,15 @@
-"""Package-wide constants."""
+# AUTO-GENERATED FILE. DO NOT EDIT MANUALLY.
+"""This is a Jinja2 auto-generated module. This module contains enums
+and constants used package-wide. The `USGSCollection` StrEnum is generated
+by inspecting the USGS OGC API JSON schema and identifying all "items" endpoints.
+
+Package version: 0.1.0
+Generation script: build_constants.py
+Generated: 2026-04-15 15:40:32 Z
+JSON Schema source: https://api.waterdata.usgs.gov/ogcapi/v0/openapi?f=json
+JSON Schema version: 0.44.0
+OpenAPI version: 3.0.2
+"""
 from enum import StrEnum
 
 class OGCAPI(StrEnum):
@@ -16,7 +27,7 @@ class OGCPATH(StrEnum):
     SCHEMA = "schema"
 
 class USGSCollection(StrEnum):
-    """USGS OGC API Collectins."""
+    """USGS OGC API Collections."""
     AGENCY_CODES = "agency-codes"
     ALTITUDE_DATUMS = "altitude-datums"
     AQUIFER_CODES = "aquifer-codes"
@@ -28,9 +39,10 @@ class USGSCollection(StrEnum):
     COORDINATE_DATUM_CODES = "coordinate-datum-codes"
     COORDINATE_METHOD_CODES = "coordinate-method-codes"
     COUNTIES = "counties"
+    COUNTRIES = "countries"
     DAILY = "daily"
-    FIELD_MEASUREMENTS_METADATA = "field-measurements-metadata"
     FIELD_MEASUREMENTS = "field-measurements"
+    FIELD_MEASUREMENTS_METADATA = "field-measurements-metadata"
     HYDROLOGIC_UNIT_CODES = "hydrologic-unit-codes"
     LATEST_CONTINUOUS = "latest-continuous"
     LATEST_DAILY = "latest-daily"
