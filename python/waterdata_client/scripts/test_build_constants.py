282ad5ae68953528970b1cffa837f4b227750117
jason.regina <jason.regina@noaa.gov>
2026-04-16 15:24:56 +0000

    handle weird collection names

--- python/waterdata_client/scripts/test_build_constants.py
+++ python/waterdata_client/scripts/test_build_constants.py
@@ -51,6 +51,20 @@ def bad_schema_digits() -> dict[str, Any]:
         }
     }
 
+@pytest.fixture
+def weird_schema() -> dict[str, Any]:
+    """Mock test schema."""
+    return {
+        "paths": {
+            "/collections/monitoring-locations/items/": {"get": {}},
+            "/collections/daily-v2.beta/items": {"get": {}},
+            "/collections/daily-v3.beta/items/": {"get": {}},
+            "/collections/123-items/items": {"get": {}},
+            "/collections/items/items": {"get": {}},
+            "/conformance": {"get": {}} 
+        }
+    }
+
 def test_get_template_data(mock_schema):
     """Verify extraction of collections."""
     data = get_template_data(mock_schema)
@@ -184,3 +198,27 @@ def test_cli_ignore_errors(bad_schema_digits, mock_template, tmp_path):
         assert "class USGSCollection" in result.output
         assert "daily" in result.output
         assert "DAILY" in result.output
+
+def test_weird_collections(weird_schema):
+    """Verify extraction of weird collections."""
+    data = get_template_data(weird_schema, ignore_errors=True)
+
+    assert len(data) == 4
+    assert data[0]["value"] == "daily-v2.beta"
+    assert data[3]["enum_member"] == "MONITORING_LOCATIONS"
+
+def test_fix_errors_default(weird_schema):
+    """Verify extraction of weird collections."""
+    data = get_template_data(weird_schema, fix_errors=True)
+
+    assert len(data) == 5
+    assert data[0]["value"] == "123-items"
+    assert data[0]["enum_member"] == "COLLECTION_123_ITEMS"
+
+def test_fix_errors_custom(weird_schema):
+    """Verify extraction of weird collections."""
+    data = get_template_data(weird_schema, fix_errors=True, error_prefix="ENDPOINT_")
+
+    assert len(data) == 5
+    assert data[0]["value"] == "123-items"
+    assert data[0]["enum_member"] == "ENDPOINT_123_ITEMS"
