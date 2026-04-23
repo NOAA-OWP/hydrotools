a9df1143c6b25e51259b6c56421a8ea9f23e63c8
jason.regina <jason.regina@noaa.gov>
2026-04-23 19:11:01 +0000

    add basic tests for API parameters

--- python/waterdata_client/scripts/test_schema_extract.py
+++ python/waterdata_client/scripts/test_schema_extract.py
@@ -15,6 +15,88 @@ def mock_schema() -> dict[str, Any]:
     }
 
 @pytest.fixture
+def mock_parameter_schema() -> dict[str, Any]:
+    """Mock test schema."""
+    return {
+        "paths": {
+            "/collections/monitoring-locations/items": {"get": {
+                "description": "MONITORING LOCATIONS",
+                "parameters": [{
+                    "description": "PARAMETER ONE",
+                    "in": "query",
+                    "name": "parameter_one",
+                    "required": False,
+                    "schema": {
+                        "default": 10_000,
+                        "type": "integer"
+                    }
+                },
+                {
+                    "description": "PARAMETER TWO",
+                    "in": "query",
+                    "name": "parameter_two",
+                    "required": True,
+                    "schema": {
+                        "default": "left",
+                        "type": "string",
+                        "enum": ["left", "right"]
+                    }
+                },
+                {
+                    "description": "PARAMETER THREE",
+                    "in": "query",
+                    "name": "parameter_three",
+                    "required": False,
+                    "schema": {
+                        "default": 4.3,
+                        "type": "number"
+                    }
+                },
+                {
+                    "description": "PARAMETER X",
+                    "in": "query",
+                    "name": "parameter_x",
+                    "required": True,
+                    "schema": {
+                        "default": True,
+                        "type": "boolean"
+                    }
+                },
+                {
+                    "description": "PARAMETER ZEE",
+                    "in": "query",
+                    "name": "parameter_zee",
+                    "required": False,
+                    "schema": {
+                        "items": {"type": "number"},
+                        "type": "array"
+                    }
+                },
+                {
+                    "description": "IGNORE ME",
+                    "in": "not_query",
+                    "name": "ignore_me",
+                    "required": True,
+                    "schema": {
+                        "items": {"type": "number"},
+                        "type": "array"
+                    }
+                },
+                {
+                    "description": "OBJECT",
+                    "in": "query",
+                    "name": "zobject_parameter",
+                    "required": True,
+                    "schema": {
+                        "default": {"key": "key", "value": 1.0},
+                        "type": "object"
+                    }
+                }]
+            }},
+        }
+    }
+
+@pytest.fixture
 def bad_schema_special() -> dict[str, Any]:
     """Bad test schema with special characters."""
     return {
@@ -57,6 +139,50 @@ def test_get_template_data(mock_schema):
     assert len(data) == 2
     assert data[0]["value"] == "daily"
     assert data[1]["enum_member"] == "MONITORING_LOCATIONS"
+
+def test_get_template_parameters(mock_parameter_schema):
+    """Verify extraction of collection parameters."""
+    data = get_template_data(mock_parameter_schema)
+
+    assert len(data) == 1
+    assert data[0]["enum_member"] == "MONITORING_LOCATIONS"
+    assert data[0]["class_name"] == "MonitoringLocationsClient"
+    assert data[0]["description"] == "MONITORING LOCATIONS"
+    assert data[0]["value"] == "monitoring-locations"
+    assert len(data[0]["parameters"]) == 6
+
+    # Check parameter one
+    assert data[0]["parameters"][0]["name"] == "parameter_one"
+    assert data[0]["parameters"][0]["python_name"] == "parameter_one"
+    assert data[0]["parameters"][0]["type_hint"] == "Optional[int]"
+    assert data[0]["parameters"][0]["default"] == 10_000
+    assert data[0]["parameters"][0]["description"] == "PARAMETER ONE"
+
+    # Check parameter three
+    assert data[0]["parameters"][1]["name"] == "parameter_three"
+    assert data[0]["parameters"][1]["python_name"] == "parameter_three"
+    assert data[0]["parameters"][1]["type_hint"] == 'Optional[float]'
+    assert data[0]["parameters"][1]["default"] == 4.3
+    assert data[0]["parameters"][1]["description"] == "PARAMETER THREE"
+
+    # Check parameter two
+    assert data[0]["parameters"][2]["name"] == "parameter_two"
+    assert data[0]["parameters"][2]["python_name"] == "parameter_two"
+    assert data[0]["parameters"][2]["type_hint"] == 'Literal["left", "right"]'
+    assert data[0]["parameters"][2]["default"] == '"left"'
+    assert data[0]["parameters"][2]["description"] == "PARAMETER TWO"
+
+    # Check parameter x
+    assert data[0]["parameters"][3]["type_hint"] == 'bool'
+    assert data[0]["parameters"][3]["default"]
+
+    # Check parameter zee
+    assert data[0]["parameters"][4]["type_hint"] == 'Optional[Sequence[float]]'
+    assert data[0]["parameters"][4]["default"] is None
+
+    # Check object parameter
+    assert data[0]["parameters"][5]["type_hint"] == 'dict'
+    assert data[0]["parameters"][5]["default"] == {"key": "key", "value": 1.0}
 
 def test_bad_schema_special(bad_schema_special):
     """Verify raises SyntaxError."""
